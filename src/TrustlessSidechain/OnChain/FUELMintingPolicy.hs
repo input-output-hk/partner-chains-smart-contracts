edbcede6fec74e903bc4a54e74bd89362658f120
Hazem ElMasry <hazem@mlabs.city>
2022-05-06 17:26:48 +0200

    Align minting policy params with the spec
    and improve naming

--- src/TrustlessSidechain/OnChain/FUELMintingPolicy.hs
+++ src/TrustlessSidechain/OnChain/FUELMintingPolicy.hs
@@ -10,11 +10,25 @@ import PlutusTx.Prelude
 
 import TrustlessSidechain.OnChain.CommitteeCandidateValidator (SidechainParams)
 
-{-# INLINEABLE mkPolicy #-}
-mkPolicy :: SidechainParams -> () -> ScriptContext -> Bool
-mkPolicy _ _ _ = True
+-- | The Redeemer that's to be passed to onchain policy, indicating its mode of usage.
+data FUELRedeemer
+  = MainToSide !BuiltinByteString -- Recipient address
+  | SideToMain -- !MerkleProof
 
-fuelMP :: SidechainParams -> MintingPolicy
-fuelMP param =
+-- Recipient address is in FUELRedeemer just for reference on the mainchain,
+-- it's actually useful (and verified) on the sidechain, so it needs to be
+-- recorded in the blockchain.
+
+makeIsDataIndexed ''FUELRedeemer [('MainToSide, 0), ('SideToMain, 1)]
+
+{-# INLINEABLE mkFUELMintingPolicy #-}
+mkFUELMintingPolicy :: SidechainParams -> FUELRedeemer -> ScriptContext -> Bool
+mkFUELMintingPolicy _ (MainToSide _) _ = True
+mkFUELMintingPolicy _ SideToMain _ = True
+
+fuelMintingPolicy :: SidechainParams -> MintingPolicy
+fuelMintingPolicy param =
   mkMintingPolicyScript
-    ($$(compile [||wrapMintingPolicy . mkPolicy||]) `applyCode` liftCode param)
+    ($$(compile [||wrap . mkFUELMintingPolicy||]) `applyCode` liftCode param)
+  where
+    wrap = wrapMintingPolicy
