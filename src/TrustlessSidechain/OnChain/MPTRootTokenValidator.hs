e53e45baf5f2138569a0054019d67227560872cc
goverthrow <goverthrow@protonmail.com>
2022-06-13 16:24:56 +0200

    MPTRootTokenValidator

---
+++ src/TrustlessSidechain/OnChain/MPTRootTokenValidator.hs
@@ -0,0 +1,38 @@
+{-# LANGUAGE TemplateHaskell #-}
+
+module TrustlessSidechain.OnChain.MPTRootTokenValidator where
+
+import Ledger (
+  ScriptContext,
+ )
+import Ledger qualified
+import Ledger.Typed.Scripts qualified as Script
+import PlutusTx (applyCode, compile, liftCode)
+import PlutusTx.Prelude
+import TrustlessSidechain.OffChain.Types (SidechainParams)
+
+data MPT
+instance Script.ValidatorTypes MPT where
+  type RedeemerType MPT = ()
+  type DatumType MPT = ()
+
+{-# INLINEABLE mkValidator #-}
+mkValidator :: SidechainParams -> () -> () -> ScriptContext -> Bool
+mkValidator _ () () _ = False
+
+typedValidator :: SidechainParams -> Script.TypedValidator MPT
+typedValidator p =
+  Script.mkTypedValidator @MPT
+    ($$(PlutusTx.compile [||mkValidator||]) `PlutusTx.applyCode` PlutusTx.liftCode p)
+    $$(PlutusTx.compile [||wrap||])
+  where
+    wrap = Script.wrapValidator @() @()
+
+validator :: SidechainParams -> Ledger.Validator
+validator = Script.validatorScript . typedValidator
+
+hash :: SidechainParams -> Ledger.ValidatorHash
+hash = Script.validatorHash . typedValidator
+
+address :: SidechainParams -> Ledger.Address
+address = Ledger.scriptHashAddress . hash
--- src/TrustlessSidechain/OnChain/Types.hs
+++ src/TrustlessSidechain/OnChain/Types.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE TypeFamilies #-}
 
 module TrustlessSidechain.OnChain.Types where
 
