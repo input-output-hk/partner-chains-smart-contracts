e1f3304e15bcec172218802c68d5cf60179ddbe2
jared <>
2022-06-17 16:03:19 -0600

    cleaned up test suite

--- test/Test/TrustlessSidechain/MerkleTree.hs
+++ test/Test/TrustlessSidechain/MerkleTree.hs
@@ -1,13 +1,14 @@
 {-# LANGUAGE TemplateHaskell #-}
 {-# OPTIONS_GHC -Wno-incomplete-uni-patterns #-}
 
--- | This module
+-- | This module is a bunch of tests for our MerkleTree implementation. For
+-- now, it's just property based tests.
 module Test.TrustlessSidechain.MerkleTree (test) where
 
 import PlutusPrelude (NonEmpty ((:|)))
 import PlutusTx.Builtins.Class qualified as Builtins
 import PlutusTx.Prelude
-import Test.QuickCheck (Arbitrary (arbitrary), Gen, Property)
+import Test.QuickCheck (Arbitrary (arbitrary), Gen, Property, Testable)
 import Test.QuickCheck qualified as QuickCheck
 import Test.QuickCheck.Modifiers (NonEmptyList (getNonEmpty))
 import Test.Tasty (TestTree)
@@ -16,58 +17,77 @@ import Test.Tasty.QuickCheck qualified as QuickCheck
 import TrustlessSidechain.MerkleTree qualified as MT
 import Prelude qualified
 
--- | 'MerkleListTestCase' new type wrapper to help generate test cases.
-newtype MerkleListTestCase = MerkleListTestCase (NonEmpty BuiltinByteString)
-  deriving stock (Prelude.Show)
+{- | 'genNonEmptyBuiltinByteString' randomly generates 'NonEmpty' lists of
+ 'BuiltinByteString'
+-}
+genNonEmptyBuiltinByteString :: Gen (NonEmpty BuiltinByteString)
+genNonEmptyBuiltinByteString = do
+  lst <- Prelude.fmap getNonEmpty (arbitrary :: Gen (NonEmptyList Prelude.String))
+  let a : as = lst
+  return $ Prelude.fmap Builtins.stringToBuiltinByteString $ a :| as
 
-instance Arbitrary MerkleListTestCase where
-  arbitrary = do
-    lst <- Prelude.fmap getNonEmpty (arbitrary :: Gen (NonEmptyList Prelude.String))
-    let a : as = lst
-    return $ MerkleListTestCase $ Prelude.fmap Builtins.stringToBuiltinByteString $ a :| as
+{- | @forAllNonEmptyBuiltinByteString prf@ is read as "for every nonempty list
+ of BuiltinByteString, @prf@ is satisified.."
+-}
+forAllNonEmptyBuiltinByteString :: Testable prop => (NonEmpty BuiltinByteString -> prop) -> Property
+forAllNonEmptyBuiltinByteString = QuickCheck.forAll genNonEmptyBuiltinByteString
 
--- TODO: implement the 'QuickCheck.shrink' function sometime.
+{- | @forAllNonEmptyBuiltinByteStringWithElem prf@ is read as "for every nonempty list @lst@, and @x \in lst@
+ of BuiltinByteString, @prf (lst, x)@ is satisified.."
+-}
+forAllNonEmptyBuiltinByteStringWithElem :: Testable prop => ((NonEmpty BuiltinByteString, BuiltinByteString) -> prop) -> Property
+forAllNonEmptyBuiltinByteStringWithElem = QuickCheck.forAll genNonEmptyWithElem
+  where
+    genNonEmptyWithElem :: Gen (NonEmpty BuiltinByteString, BuiltinByteString)
+    genNonEmptyWithElem = do
+      a :| as <- genNonEmptyBuiltinByteString
+      x <- QuickCheck.elements $ a : as
+      return (a :| as, x)
+
+{- | @forAllNonEmptyBuiltinByteStringWithoutElem prf@ is read as "for every nonempty list @lst@, and @x \not \in lst@
+ of BuiltinByteString, @prf (lst, x)@ is satisified.."
+-}
+forAllNonEmptyBuiltinByteStringWithoutElem :: Testable prop => ((NonEmpty BuiltinByteString, BuiltinByteString) -> prop) -> Property
+forAllNonEmptyBuiltinByteStringWithoutElem = QuickCheck.forAll genNonEmptyWithoutElem
+  where
+    genNonEmptyWithoutElem :: Gen (NonEmpty BuiltinByteString, BuiltinByteString)
+    genNonEmptyWithoutElem = do
+      a :| as <- genNonEmptyBuiltinByteString
+      x <- Prelude.fmap Builtins.stringToBuiltinByteString (QuickCheck.arbitrary :: Gen Prelude.String)
+      if x `elem` (a : as)
+        then QuickCheck.discard
+        else return (a :| as, x)
 
 {- | Property.
     Let lst be an arbitrary non empty list.
         height (fromNonEmpty lst) <= floor(log_2 (length lst)) + 2
 -}
-prop_logProofLength :: MerkleListTestCase -> Property
-prop_logProofLength (MerkleListTestCase lst@(a :| as)) =
-  QuickCheck.property $
-    MT.height tree
-      <= Prelude.floor
-        (Prelude.logBase @Prelude.Double 2 (Prelude.fromIntegral (length lst')))
-      + 2
-  where
-    tree = MT.fromNonEmpty lst
-    lst' = a : as
+prop_logProofLength :: Property
+prop_logProofLength = forAllNonEmptyBuiltinByteString $
+  \lst@(a :| as) ->
+    let lst' = a : as
+        tree = MT.fromNonEmpty lst
+     in MT.height tree <= Prelude.floor (Prelude.logBase @Prelude.Double 2 (Prelude.fromIntegral (length lst'))) + 2
 
 {- | Property.
   x \in lst ==> isJust (MT.lookupMP x (MT.fromNonEmpty lst))
 -}
-prop_lookupMPHasProof :: MerkleListTestCase -> Property
-prop_lookupMPHasProof (MerkleListTestCase lst@(a :| as)) =
-  QuickCheck.property $
-    all
-      (isJust . flip MT.lookupMP tree)
-      lst'
-  where
-    tree = MT.fromNonEmpty lst
-    lst' = a : as
+prop_lookupMPHasProof :: Property
+prop_lookupMPHasProof =
+  forAllNonEmptyBuiltinByteStringWithElem $
+    \(lst@(_a :| _as), x) -> isJust $ MT.lookupMP x $ MT.fromNonEmpty lst
 
 {- | Property.
   x \in lst <== isJust (MT.lookupMP x (MT.fromNonEmpty lst))
 
  but we test this via the contrapositive.
 -}
-prop_notInLookupMPFail :: Prelude.String -> MerkleListTestCase -> Property
-prop_notInLookupMPFail str (MerkleListTestCase lst@(a :| as)) =
-  (str' `notElem` lst') QuickCheck.==> isNothing (MT.lookupMP str' tree)
-  where
-    tree = MT.fromNonEmpty lst
-    str' = Builtins.stringToBuiltinByteString str
-    lst' = a : as
+prop_notInLookupMPFail :: Property
+prop_notInLookupMPFail =
+  forAllNonEmptyBuiltinByteStringWithoutElem $
+    \(lst@(_ :| _), x) ->
+      let tree = MT.fromNonEmpty lst
+       in isNothing (MT.lookupMP x tree)
 
 {- | Property.
  Suppose lst is an arbitrary non empty list.
@@ -75,18 +95,15 @@ prop_notInLookupMPFail str (MerkleListTestCase lst@(a :| as)) =
 
   Just prf = lookupMP x tree ==> memberMP x prf (rootHash tree) = True
 
- TODO: The converse of this is trickier to test, and the large search space
+ TODO: Didn't test the converse -- it's a bit trickier to test, and the large search space
  makes me doubt the usefulness of QuickCheck for this.
 -}
-prop_inListMemberMP :: MerkleListTestCase -> Property
-prop_inListMemberMP (MerkleListTestCase lst@(a :| as)) =
-  QuickCheck.property $
-    all
-      (\x -> let Just prf = MT.lookupMP x tree in MT.memberMP x prf (MT.rootHash tree))
-      lst'
-  where
-    tree = MT.fromNonEmpty lst
-    lst' = a : as
+prop_inListMemberMP :: Property
+prop_inListMemberMP =
+  forAllNonEmptyBuiltinByteStringWithElem $ \(lst@(_a :| _as), x) ->
+    let tree = MT.fromNonEmpty lst
+        Just prf = MT.lookupMP x tree
+     in MT.memberMP x prf (MT.rootHash tree)
 
 -- This is needed because of QuickCheck. It's explained in the QuickCheck
 -- documentation.
