3a7b72f2f6cbc68b6edc3b79dc6dddcd8b544f11
jared <>
2022-06-20 14:19:38 -0600

    Shuffled around documentation

--- src/TrustlessSidechain/MerkleTree.hs
+++ src/TrustlessSidechain/MerkleTree.hs
@@ -209,54 +209,11 @@ fromNonEmpty :: NonEmpty BuiltinByteString -> MerkleTree
 fromNonEmpty = fromList . PlutusPrelude.toList
 
 {-
-N.B. This technique of going "bottom up" to create the tree is well known
-when implementing merge sort
-    [1] See 'Data.List.sort'
-    [2] "When You Should Use Lists In Haskell (Mostly You Should Not)" by Johannes Waldmann
-    [3] "Algorithm Design in Haskell" by Richard Bird / Jeremy Gibbons
-
-Properties.
+Properties [Merkle Tree Height Bound]
     Let lst be an arbitrary non empty list.
         height (fromNonEmpty lst) <= floor(log_2 (length lst)) + 2
 
-Practically, here are some benchmarks from the hydra-poc (who use a different
-method to build the tree -- they go top down): The hydra-poc guys have:
-
-| Size | % member max mem | % member max cpu | % builder max mem | % builder max cpu |
-| :--- | ---------------: | ---------------: | ----------------: | ----------------: |
-| 1    | 2.44             | 1.55             | 2.66              | 1.64              |
-| 2    | 2.58             | 1.66             | 3.31              | 2.09              |
-| 5    | 2.77             | 1.82             | 8.22              | 5.53              |
-| 10   | 2.91             | 1.93             | 25.49             | 17.62             |
-| 20   | 3.04             | 2.04             | 94.54             | 65.96             |
-| 50   | 3.22             | 2.19             | 0.00              | 0.00              |
-| 100  | 3.36             | 2.30             | 0.00              | 0.00              |
-| 500  | 3.66             | 2.56             | 0.00              | 0.00              |
-Benchmark on-chain-cost: FINISH
-
-And our method has running time like:
-| Size | % member max mem | % member max cpu | % builder max mem | % builder max cpu |
-| :--- | ---------------: | ---------------: | ----------------: | ----------------: |
-| 1    | 2.44             | 1.55             | 2.57              | 1.60              |
-| 2    | 2.58             | 1.66             | 2.76              | 1.74              |
-| 5    | 2.79             | 1.84             | 3.31              | 2.16              |
-| 10   | 2.93             | 1.95             | 4.08              | 2.80              |
-| 20   | 3.06             | 2.06             | 5.58              | 4.06              |
-| 50   | 3.24             | 2.20             | 9.98              | 7.81              |
-| 100  | 3.37             | 2.32             | 17.25             | 14.04             |
-| 500  | 3.66             | 2.55             | 75.18             | 63.70             |
-Benchmark on-chain-cost: FINISH
-
-where 'member' is 'memberMP' here and 'builder' is 'fromList'.
-
-We see that the bottom up implementation of 'fromList' is almost 10x more
-efficient on some cases, and I'm fairly certain the last 3 cases of the
-hydra-poc guys are botched and didn't actually run whereas our implementation
-actually did run.
-
-I'll note that the hydra-poc people have a slightly more efficient 'memberMP'
-I suspect this is just "lucky" with the test cases. Both trees are bound
-logarithmically by the same height.
+See: Note [Hydra-Poc People Merkle Tree Comparisons]
 -}
 
 {- | /O(log n)/. Lookup the the corresponding merkle proof of the
@@ -318,6 +275,8 @@ lookupMP bt mt = fmap MerkleProof $ go [] mt
 
     2.
         Just prf = lookupMp x (fromNonEmpty lst) ==> length prf <= floor (log_2 (length lst)) + 1
+
+    N.B. 2. follows from [Merkle Tree Height Bound].
 -}
 
 {- | /O(n)/ in the length of the 'MerkleProof' (which is /O(log n)/ of
@@ -344,4 +303,110 @@ memberMP bt prf rth = rth == go (hash bt) (unMerkleProof prf)
     Suppose lst is an arbitrary non empty list.
     Let tree = fromNonEmpty lst
         Just prf = lookupMP x tree <==> memberMP x prf (rootHash tree) = True
+-}
+
+{-
+Note [Hydra-Poc People Merkle Tree Comparisons]
+
+We discuss some comparisions between us and the hydra-poc people. The main
+difference between us and hydra-poc people is how the merkle tree is actually
+constructed. They do a top down approach (compute the length of the list,
+divide by 2, then split the list into the first half / second half, then keep
+going), where as we do a "bottom up" approach.
+
+N.B. This technique of going "bottom up" to create the tree is well known
+when implementing merge sort
+    [1] See 'Data.List.sort'
+    [2] "When You Should Use Lists In Haskell (Mostly You Should Not)" by Johannes Waldmann
+    [3] "Algorithm Design in Haskell" by Richard Bird / Jeremy Gibbons
+
+The hydra-poc people claim their tree has an exact height upper bound as given
+in [Merkle Tree Height Bound]. It's a bit of a peculier choice of constants
+[this is implied in their QuickCheck tests], but if we want our implementation
+to be at least as good as their solution, we should show that our trees are
+bound by the same bound.
+
+We sketch out why this is the case.
+
+Observation 1.
+    The height of a merkle tree is upper bounded by the recurrence
+
+    T(n) = 1                    if n = 1
+           T(ceil(n/2)) + 1     otherwise
+
+    since this counts the number of times the mergePairs function gets called
+    in 'fromList' where we may observe that the height of the tree increases by
+    iff mergePairs gets called.
+
+Thus, it suffices to show
+
+    T(n) <= floor(log_2(n)) + 2
+
+for every n >= 1.
+
+Claim 1.
+    for every k >= 0
+    T(2^k) = log_2(n) + 1
+
+Proof.
+    By induction on k.
+
+Claim 2.
+
+    T(n) <= floor(log_2(n)) + 2
+
+for every n >= 1.
+
+Proof. By Claim 1. we only need to consider the case when n is not a power of
+2. In which case we may write
+    2^k < n
+where k is the largest integer satisfying this inequality.
+It follows that
+    2^k / 2 < ceil(n/2) <= 2^k < n          (*)
+and observing that the recurrence T is obviously increasing, we get that
+    T(n) = T(ceil(n/2)) + 1
+         <= T(2^k) + 1          [T is increasing]
+         = (log_2 2^k + 1) + 1  [Claim 1]
+         = log_2 2^k + 2
+         <= floor(log_2 n) + 2          [Apply (*)]
+as required.
+
+Practically, here are some benchmarks from the hydra-poc. The hydra-poc people
+have benchmarks like:
+
+| Size | % member max mem | % member max cpu | % builder max mem | % builder max cpu |
+| :--- | ---------------: | ---------------: | ----------------: | ----------------: |
+| 1    | 2.44             | 1.55             | 2.66              | 1.64              |
+| 2    | 2.58             | 1.66             | 3.31              | 2.09              |
+| 5    | 2.77             | 1.82             | 8.22              | 5.53              |
+| 10   | 2.91             | 1.93             | 25.49             | 17.62             |
+| 20   | 3.04             | 2.04             | 94.54             | 65.96             |
+| 50   | 3.22             | 2.19             | 0.00              | 0.00              |
+| 100  | 3.36             | 2.30             | 0.00              | 0.00              |
+| 500  | 3.66             | 2.56             | 0.00              | 0.00              |
+Benchmark on-chain-cost: FINISH
+
+And our method has running time like:
+| Size | % member max mem | % member max cpu | % builder max mem | % builder max cpu |
+| :--- | ---------------: | ---------------: | ----------------: | ----------------: |
+| 1    | 2.44             | 1.55             | 2.57              | 1.60              |
+| 2    | 2.58             | 1.66             | 2.76              | 1.74              |
+| 5    | 2.79             | 1.84             | 3.31              | 2.16              |
+| 10   | 2.93             | 1.95             | 4.08              | 2.80              |
+| 20   | 3.06             | 2.06             | 5.58              | 4.06              |
+| 50   | 3.24             | 2.20             | 9.98              | 7.81              |
+| 100  | 3.37             | 2.32             | 17.25             | 14.04             |
+| 500  | 3.66             | 2.55             | 75.18             | 63.70             |
+Benchmark on-chain-cost: FINISH
+
+where 'member' is 'memberMP' here and 'builder' is 'fromList'.
+
+We see that the bottom up implementation of 'fromList' is almost 10x more
+efficient on some cases, and I'm fairly certain the last 3 cases of the
+hydra-poc guys are botched and didn't actually run whereas our implementation
+actually did run.
+
+I'll note that the hydra-poc people have a slightly more efficient 'memberMP'
+I suspect this is just "lucky" with the test cases because in some cases our
+member function is faster as well. This follows from [Merkle Tree Height Bound].
 -}
