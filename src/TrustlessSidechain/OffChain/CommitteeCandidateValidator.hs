88933ef38c98eceb0c3f17597110b1eb8c0d9f42
Szabo Gergely <gege251@mailbox.org>
2022-06-20 20:38:35 +0200

    Verify deregister pkh

--- src/TrustlessSidechain/OffChain/CommitteeCandidateValidator.hs
+++ src/TrustlessSidechain/OffChain/CommitteeCandidateValidator.hs
@@ -5,10 +5,11 @@ module TrustlessSidechain.OffChain.CommitteeCandidateValidator where
 import Control.Monad (when)
 import Data.Map qualified as Map
 import Data.Text (Text)
+import Ledger (PaymentPubKeyHash (PaymentPubKeyHash, unPaymentPubKeyHash))
 import Ledger qualified
 import Ledger.Ada qualified as Ada
 import Ledger.Constraints qualified as Constraints
-import Ledger.Crypto (Signature (getSignature), getPubKey)
+import Ledger.Crypto (PubKeyHash)
 import Ledger.Scripts qualified as Scripts
 import Ledger.Tx (CardanoTx, ChainIndexTxOut (PublicKeyChainIndexTxOut, ScriptChainIndexTxOut), TxOutRef)
 import Ledger.Typed.Scripts (
@@ -16,15 +17,13 @@ import Ledger.Typed.Scripts (
  )
 import Ledger.Typed.Scripts qualified as Scripts
 import Plutus.Contract (Contract, ownPaymentPubKeyHash, submitTxConstraintsWith, throwError, utxosAt)
-import Plutus.V1.Ledger.Api (LedgerBytes (getLedgerBytes))
 import Plutus.V1.Ledger.Scripts (Datum (Datum))
 import PlutusTx qualified
 import PlutusTx.Prelude hiding (Semigroup ((<>)))
 import TrustlessSidechain.OffChain.Schema (TrustlessSidechainSchema)
 import TrustlessSidechain.OffChain.Types (DeregisterParams (..), RegisterParams (..))
 import TrustlessSidechain.OnChain.CommitteeCandidateValidator (
-  BlockProducerRegistration (BlockProducerRegistration, bprInputUtxo, bprSidechainPubKey, bprSpoPubKey, bprSpoSignature),
-  BlockProducerRegistrationMsg (BlockProducerRegistrationMsg),
+  BlockProducerRegistration (BlockProducerRegistration, bprOwnPkh, bprSpoPubKey),
   CommitteeCandidateRegistry,
  )
 import TrustlessSidechain.OnChain.CommitteeCandidateValidator qualified as CommitteeCandidateValidator
@@ -51,7 +50,14 @@ register RegisterParams {sidechainParams, spoPubKey, sidechainPubKey, spoSig, si
       lookups =
         Constraints.unspentOutputs ownUtxos
           <> Constraints.typedValidatorLookups validator
-      datum = BlockProducerRegistration spoPubKey sidechainPubKey spoSig sidechainSig inputUtxo
+      datum =
+        BlockProducerRegistration
+          spoPubKey
+          sidechainPubKey
+          spoSig
+          sidechainSig
+          inputUtxo
+          (unPaymentPubKeyHash ownPkh)
       tx = Constraints.mustPayToTheScript datum val <> Constraints.mustSpendPubKeyOutput inputUtxo
 
   submitTxConstraintsWith lookups tx
@@ -66,7 +72,7 @@ deregister DeregisterParams {sidechainParams, spoPubKey} = do
   ownUtxos <- utxosAt ownAddr
   valUtxos <- utxosAt valAddr
 
-  let ownEntries = Map.filter isOwnEntry valUtxos
+  let ownEntries = Map.filter (isOwnEntry ownPkh) valUtxos
 
       lookups =
         Constraints.otherScript (Scripts.validatorScript validator)
@@ -83,21 +89,16 @@ deregister DeregisterParams {sidechainParams, spoPubKey} = do
 
   submitTxConstraintsWith @CommitteeCandidateRegistry lookups tx
   where
-    isOwnEntry :: ChainIndexTxOut -> Bool
-    isOwnEntry PublicKeyChainIndexTxOut {} = False
-    isOwnEntry ScriptChainIndexTxOut {_ciTxOutDatum = Left _} = False
-    isOwnEntry ScriptChainIndexTxOut {_ciTxOutDatum = Right (Datum d)} =
-      maybe False isSignatureValid (PlutusTx.fromBuiltinData d)
+    isOwnEntry :: PaymentPubKeyHash -> ChainIndexTxOut -> Bool
+    isOwnEntry _ PublicKeyChainIndexTxOut {} = False
+    isOwnEntry _ ScriptChainIndexTxOut {_ciTxOutDatum = Left _} = False
+    isOwnEntry (PaymentPubKeyHash pkh) ScriptChainIndexTxOut {_ciTxOutDatum = Right (Datum d)} =
+      maybe False (isOwnEntry' pkh) (PlutusTx.fromBuiltinData d)
 
-    isSignatureValid :: BlockProducerRegistration -> Bool
-    isSignatureValid datum =
-      let sidechainPubKey = bprSidechainPubKey datum
-          inputUtxo = bprInputUtxo datum
-          pubKey = getLedgerBytes $ getPubKey $ bprSpoPubKey datum
-          sig = getSignature $ bprSpoSignature datum
-
-          msg = CommitteeCandidateValidator.serialiseBprm $ BlockProducerRegistrationMsg sidechainParams sidechainPubKey inputUtxo
-       in spoPubKey == bprSpoPubKey datum && verifySignature pubKey msg sig
+    isOwnEntry' :: PubKeyHash -> BlockProducerRegistration -> Bool
+    isOwnEntry' pkh datum =
+      bprSpoPubKey datum == spoPubKey
+        && bprOwnPkh datum == pkh
 
 registerWithMock :: RegisterParams -> Contract () TrustlessSidechainSchema Text CardanoTx
 registerWithMock =
