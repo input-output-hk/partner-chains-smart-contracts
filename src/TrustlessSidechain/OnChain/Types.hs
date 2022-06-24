e5fbed508e19694644472356835f76661ec46999
jared <>
2022-06-24 16:33:43 -0600

    Added Merkle Proof to offchain code
    Still WIP:
        - On chain code doesn't use the Merkle Proof at all -- this is a bit
          blocked since we don't have reference outputs yet
        - Distributed set
        - The test suite will fail since the old tests didn't need to locate
          MPTRootTokens, and now the off chain code must locate such tokens

--- src/TrustlessSidechain/OnChain/Types.hs
+++ src/TrustlessSidechain/OnChain/Types.hs
@@ -7,13 +7,14 @@ import Ledger.Crypto (PubKey)
 import Ledger.Typed.Scripts qualified as Script
 import PlutusTx (makeIsDataIndexed)
 import PlutusTx.Prelude (BuiltinByteString)
+import TrustlessSidechain.MerkleTree (MerkleProof)
 
 -- | The Redeemer that's to be passed to onchain policy, indicating its mode of usage.
 data FUELRedeemer
   = MainToSide
       !BuiltinByteString -- Recipient's sidechain address
       !BuiltinByteString -- Recipient's sidechain signature
-  | SideToMain -- !MerkleProof
+  | SideToMain !MerkleProof
 
 -- Recipient address is in FUELRedeemer just for reference on the mainchain,
 -- it's actually useful (and verified) on the sidechain, so it needs to be
