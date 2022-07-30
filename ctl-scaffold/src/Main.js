a79a07fef538072ab31981838e93394f934a2320
Hazem ElMasry <hazem@mlabs.city>
2022-07-31 00:31:32 +0200

    Add brackets to require (oops)
    A little bit of Lua snuck in this JS file

--- ctl-scaffold/src/Main.js
+++ ctl-scaffold/src/Main.js
@@ -2,10 +2,10 @@ var script = 'Scripts/FUELMintingPolicy.plutus';
 if (typeof BROWSER_RUNTIME !== 'undefined' && BROWSER_RUNTIME) {
     script = require(script);
 } else {
-    const fs = require 'fs';
-    const path = require 'path';
+    const fs = require('fs');
+    const path = require('path');
     script = fs.readFileSync(
-        path.resolve(__dirname, '../' + script),
+        path.resolve(__dirname, '../../' + script),
         'utf8'
     );
 }
