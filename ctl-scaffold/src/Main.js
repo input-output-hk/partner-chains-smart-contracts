8e2dfe78cc06568d20e6b1492fb352971ac535ad
Hazem ElMasry <hazem@mlabs.city>
2022-08-01 17:57:58 +0200

    Remove dependency-is-expression warning

--- ctl-scaffold/src/Main.js
+++ ctl-scaffold/src/Main.js
@@ -1,11 +1,11 @@
-var script = 'Scripts/FUELMintingPolicy.plutus';
+var script;
 if (typeof BROWSER_RUNTIME !== 'undefined' && BROWSER_RUNTIME) {
-    script = require(script);
+    script = require('Scripts/FUELMintingPolicy.plutus');
 } else {
     const fs = require('fs');
     const path = require('path');
     script = fs.readFileSync(
-        path.resolve(__dirname, '../../' + script),
+        path.resolve(__dirname, '../../Scripts/FUELMintingPolicy.plutus'),
         'utf8'
     );
 }
