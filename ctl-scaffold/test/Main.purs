83cf47f30386ecde8113faeb3562069dc460f0df
Hazem ElMasry <hazem@mlabs.city>
2022-08-02 22:15:41 +0200

    Change config to not use any defaults
    It actually works!!

--- ctl-scaffold/test/Main.purs
+++ ctl-scaffold/test/Main.purs
@@ -2,35 +2,45 @@ module Test.Main (main) where
 
 import Contract.Prelude
 
-import Contract.Config
-  ( defaultDatumCacheWsConfig
-  , defaultOgmiosWsConfig
-  , defaultServerConfig
-  )
 import Contract.Log (logInfo')
 import Contract.Monad (launchAff_)
 import Contract.Test.Plutip (PlutipConfig, runPlutipContract)
 import Data.UInt (fromInt)
-import Main (contract)
+-- import Main (contract)
 
 config ∷ PlutipConfig
 config =
-  { host: "localhost"
-  , port: fromInt 9080
-  , logLevel: Debug
-  , ogmiosConfig: defaultOgmiosWsConfig
-  , ogmiosDatumCacheConfig: defaultDatumCacheWsConfig
-  , ctlServerConfig: defaultServerConfig
-  , postgresConfig: -- `defaultConfig.postgres` in `flake.nix`
-      { dbname: "ctxlib"
-      , host: "localhost"
-      , port: fromInt 5432
+  { host: "127.0.0.1"
+  , port: fromInt 8082
+  , logLevel: Trace
+  , ogmiosConfig:
+      { port: fromInt 1338
+      , host: "127.0.0.1"
+      , secure: false
+      , path: Nothing
+      }
+  , ogmiosDatumCacheConfig:
+      { port: fromInt 10000
+      , host: "127.0.0.1"
+      , secure: false
+      , path: Nothing
+      }
+  , ctlServerConfig:
+      { port: fromInt 8083
+      , host: "127.0.0.1"
+      , secure: false
+      , path: Nothing
+      }
+  , postgresConfig:
+      { host: "127.0.0.1"
+      , port: fromInt 5433
       , user: "ctxlib"
       , password: "ctxlib"
+      , dbname: "ctxlib"
       }
   }
 
 main ∷ Effect Unit
 main = launchAff_ $ runPlutipContract config unit \_ -> do
   logInfo' "Hello World"
-  contract
+--   contract
