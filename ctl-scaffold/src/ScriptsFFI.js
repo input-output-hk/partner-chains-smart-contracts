034ff87f0d97d6e5dd1c14719c7d7e5a50245cce
James Faure <james@mlabs.city>
2022-08-08 12:47:03 +0200

    js let declaration instead of const for branch assignment

--- ctl-scaffold/src/ScriptsFFI.js
+++ ctl-scaffold/src/ScriptsFFI.js
@@ -1,29 +1,35 @@
 /* global BROWSER_RUNTIME */
 
-var inBrowser = typeof BROWSER_RUNTIME !== 'undefined' && BROWSER_RUNTIME;
+let inBrowser = typeof BROWSER_RUNTIME !== 'undefined' && BROWSER_RUNTIME;
+let fuelMintingPolicy;
+let CommitteCandidateValidator;
+let MPTRootMintingPolicy;
+let MPTRootTokenMintingPolicy;
+let MPTRootTokenValidator;
+let UpdateCommitteeHash;
 
 if (inBrowser) {
-  const fuelMintingPolicy          = require('Scripts/FUELMintingPolicy.plutus');
-  const CommitteCandidateValidator = require("../../Scripts/CommitteCandidateValidator.plutus");
-  const MPTRootMintingPolicy       = require("../../Scripts/MPTRootMintingPolicy.plutus");
-  const MPTRootTokenMintingPolicy  = require("../../Scripts/MPTRootTokenMintingPolicy.plutus");
-  const MPTRootTokenValidator      = require("../../Scripts/MPTRootTokenValidator.plutus");
-  const UpdateCommitteeHash        = require("../../Scripts/UpdateCommitteeHash.plutus");
+  fuelMintingPolicy          = require('Scripts/FUELMintingPolicy.plutus');
+  CommitteCandidateValidator = require("../../Scripts/CommitteCandidateValidator.plutus");
+  MPTRootMintingPolicy       = require("../../Scripts/MPTRootMintingPolicy.plutus");
+  MPTRootTokenMintingPolicy  = require("../../Scripts/MPTRootTokenMintingPolicy.plutus");
+  MPTRootTokenValidator      = require("../../Scripts/MPTRootTokenValidator.plutus");
+  UpdateCommitteeHash        = require("../../Scripts/UpdateCommitteeHash.plutus");
 
 } else {
   const fs = require("fs");
   const path = require("path");
-  var fuelMintingPolicy = fs.readFileSync(path.resolve(__dirname
+  fuelMintingPolicy = fs.readFileSync(path.resolve(__dirname
     , "../../Scripts/FUELMintingPolicy.plutus"), "utf8");
-  var CommitteCandidateValidator = fs.readFileSync(path.resolve(__dirname
+  CommitteCandidateValidator = fs.readFileSync(path.resolve(__dirname
     , "../../Scripts/CommitteCandidateValidator.plutus") , "utf8");
-  var MPTRootMintingPolicy = fs.readFileSync(path.resolve(__dirname
+  MPTRootMintingPolicy = fs.readFileSync(path.resolve(__dirname
     , "../../Scripts/MPTRootMintingPolicy.plutus") , "utf8");
-  var MPTRootTokenMintingPolicy = fs.readFileSync(path.resolve(__dirname
+  MPTRootTokenMintingPolicy = fs.readFileSync(path.resolve(__dirname
     , "../../Scripts/MPTRootTokenMintingPolicy.plutus") , "utf8");
-  var MPTRootTokenValidator = fs.readFileSync(path.resolve(__dirname
+  MPTRootTokenValidator = fs.readFileSync(path.resolve(__dirname
     , "../../Scripts/MPTRootTokenValidator.plutus") , "utf8");
-  var UpdateCommitteeHash = fs.readFileSync(path.resolve(__dirname
+  UpdateCommitteeHash = fs.readFileSync(path.resolve(__dirname
     , "../../Scripts/UpdateCommitteeHash.plutus") , "utf8");
 }
 exports.fUELMintingPolicy          = fuelMintingPolicy         
