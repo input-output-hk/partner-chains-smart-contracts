cebfdde4d97baef150b6479a96acf0ffb09d1b63
James Faure <james@mlabs.city>
2022-08-30 16:28:02 +0200

    ctl MPTRootTokenMintingPolicy close #104

--- ctl/src/MPTRoot.purs
+++ ctl/src/MPTRoot.purs
@@ -1,37 +1,37 @@
 module MPTRoot where
-{-
 import SidechainParams (SidechainParams)
-import ScriptsFFI (fUELMintingPolicy)
-import Data.BigInt as BigInt
+import RawScripts (rawMPTRootTokenValidator , rawMPTRootTokenMintingPolicy)
+import Data.BigInt (BigInt , fromInt)
 import Contract.Prelude (class Generic, Unit, Void, bind, discard, negate, one, show, wrap, zero, (<$>), (<>), (=<<), (>>>))
-import Contract.Log (logInfo)
-import Contract.Monad (Contract, liftContractAffM, liftContractM, liftedE, liftedM)
+import Contract.Log (logInfo')
+import Contract.Monad (Contract, liftContractM, liftedE, liftedM)
 import Contract.Prim.ByteArray (byteArrayFromAscii)
-import Contract.Scripts (MintingPolicy(..), PlutusScript(..), applyArgs)
+import Contract.Scripts (Validator(..), MintingPolicy(..), applyArgs, validatorHash)
 import Contract.TextEnvelope (TextEnvelopeType(..), textEnvelopeBytes)
 import Contract.Value as Value
 import Contract.Transaction ( awaitTxConfirmed , balanceAndSignTx , submit)
 import Contract.TxConstraints as Constraints
 import Contract.ScriptLookups as Lookups
-import Contract.PlutusData (class ToData , toData , PlutusData(Constr))
+import Contract.PlutusData (class ToData , toData , PlutusData(Constr) , unitDatum)
 import Contract.Address (PaymentPubKeyHash)
+import Types.Scripts (plutusV2Script)
 
 data SignedMerkleRoot = SignedMerkleRoot
   { merkleRoot       ∷ String
-  , signatures       ∷ [String]
+  , signatures       ∷ Array String
   , threshold        ∷ BigInt -- Natural: the number of committee pubkeys needed to sign off
-  , committeePubKeys ∷ [PubKey] -- Public keys of all committee members
+  , committeePubKeys ∷ Array PaymentPubKeyHash -- PubKey -- Public keys of all committee members
   }
 derive instance Generic SignedMerkleRoot _
 instance ToData SignedMerkleRoot where
-  toData (SignedMerkleRoot s1 s2) = Constr zero [ toData s1 , toData s2 ]
+  toData (SignedMerkleRoot {merkleRoot , signatures , threshold , committeePubKeys}) = Constr zero [ toData merkleRoot , toData signatures , toData threshold , toData committeePubKeys ]
 
 newtype SaveRootParams = SaveRootParams
   { sidechainParams  ∷ SidechainParams
   , merkleRoot       ∷ String
-  , signatures       ∷ [String]
+  , signatures       ∷ Array String
   , threshold        ∷ BigInt
-  , committeePubKeys ∷ [PubKey] -- Public keys of all committee members
+  , committeePubKeys ∷ Array PaymentPubKeyHash -- PubKey -- Public keys of all committee members
   }
 derive instance Generic SaveRootParams _
 derive instance Newtype SaveRootParams _
@@ -42,34 +42,31 @@ instance ToData SaveRootParams where
 
 rootTokenMintingPolicy ∷ SidechainParams → Contract () MintingPolicy
 rootTokenMintingPolicy sp = do
-  mptRootMP ← (PlutusScript >>> MintingPolicy) <$> textEnvelopeBytes mPTRootMintingPolicy PlutusScriptV2
+  mptRootMP ← (plutusV2Script >>> MintingPolicy) <$> textEnvelopeBytes rawMPTRootTokenMintingPolicy PlutusScriptV2
   liftedE (applyArgs mptRootMP [ toData sp ])
 
-saveRoot ∷ SaveRootParams → Contract () ()
-saveRoot SaveRootParams {sidechainParams, merkleRoot, threshold, signatures, committeePubKeys} = do
+rootTokenValidator ∷ SidechainParams → Contract () Validator
+rootTokenValidator sp = do
+  mptRootVal ← (plutusV2Script >>> Validator) <$> textEnvelopeBytes rawMPTRootTokenValidator PlutusScriptV2
+  liftedE (applyArgs mptRootVal [ toData sp ])
+
+saveRoot ∷ SaveRootParams → Contract () Unit
+saveRoot (SaveRootParams {sidechainParams, merkleRoot, threshold, signatures, committeePubKeys}) = do
   rootTokenMP ← rootTokenMintingPolicy sidechainParams
-  rootTokenCS ← liftContractAffM "Cannot get currency symbol" (Value.scriptCurrencySymbol rootTokenMP)
-  let policy   = MintingPolicy.mintingPolicy sidechainParams
-      value    = Value.singleton rootTokenCS (Value.TokenName merkleRoot) (BigInt.fromInt 1)
+  rootTokenCS ← liftContractM "Cannot get currency symbol" (Value.scriptCurrencySymbol rootTokenMP)
+  rootTokenVal← rootTokenValidator sidechainParams
+  tn          ← liftContractM "Cannot get token name" (Value.mkTokenName =<< byteArrayFromAscii merkleRoot)
+  let value    = Value.singleton rootTokenCS tn (fromInt 1)
       redeemer = SignedMerkleRoot {merkleRoot, signatures, threshold, committeePubKeys}
       constraints ∷ Constraints.TxConstraints Void Void
-      constraints = Constraint.mustMintValueWithRedeemer (wrap (toData redeemer)) value
-                 <> Constraint.mustPayToOtherScript (Validator.hash sidechainParams) Ledger.unitDatum value
-  ubTx ← liftedE (Lookups.mkUnbalancedTx (Lookups.mintingPolicy rootTokenMP) constraints)
-  bsTx ← liftedM "Failed to balance/sign tx" (balanceAndSignTx ubTx)
+      constraints = Constraints.mustMintValueWithRedeemer (wrap (toData redeemer)) value
+                 <> Constraints.mustPayToScript (validatorHash rootTokenVal) unitDatum value
+      lookups ∷ Lookups.ScriptLookups Void
+      lookups = Lookups.mintingPolicy rootTokenMP
+  ubTx ← liftedE (Lookups.mkUnbalancedTx lookups constraints)
+--bsTx ← liftedM "Failed to balance/sign tx" (balanceAndSignTx ubTx)
+  bsTx ← balanceAndSignTx ubTx
   txId ← submit bsTx
-  logInfo ("Submitted saveRoot Tx: " <> show txId)
+  logInfo' ("Submitted saveRoot Tx: " <> show txId)
   awaitTxConfirmed txId
-  logInfo "saveRoot Tx submitted successfully!"
-
---saveRoot ∷ SaveRootParams → Contract () TrustlessSidechainSchema Text CardanoTx
---saveRoot SaveRootParams {sidechainParams, merkleRoot, threshold, signatures, committeePubKeys} = do
---  let policy = MintingPolicy.mintingPolicy sidechainParams
---      value = Value.singleton (Ledger.scriptCurrencySymbol policy) (Value.TokenName merkleRoot) 1
---      redeemer = Redeemer $ toBuiltinData SignedMerkleRoot {merkleRoot, signatures, threshold, committeePubKeys}
---  Contract.submitTxConstraintsWith @SignedMerkleRoot
---    (Constraint.mintingPolicy policy)
---    ( Constraint.mustMintValueWithRedeemer redeemer value
---        <> Constraint.mustPayToOtherScript (Validator.hash sidechainParams) Ledger.unitDatum value
---    )
--}
+  logInfo' "saveRoot Tx submitted successfully!"
