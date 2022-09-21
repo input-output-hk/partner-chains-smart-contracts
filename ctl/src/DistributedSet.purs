d7f128b3c8edb2534691ce9fbd9fbe5a64d50796
jared <>
2022-09-20 22:01:40 -0600

    Started adding the initialization of the distributed set to the initialize sidechain endpoint

---
+++ ctl/src/DistributedSet.purs
@@ -0,0 +1,111 @@
+-- | This module implements the required offchain functionality of the
+-- distributed set.
+module DistributedSet
+  ( Ds(Ds)
+  , DsDatum(DsDatum)
+  , DsConfDatum(DsConfDatum)
+  , DsConfMint(DsConfMint)
+
+
+  , insertValidator
+  ) where
+
+import Contract.Prelude
+
+import Cardano.TextEnvelope (TextEnvelopeType(PlutusScriptV2))
+import Contract.Monad (Contract)
+import Contract.PlutusData (PlutusData(..))
+import Contract.Prim.ByteArray (ByteArray)
+import Contract.Scripts (Validator, ValidatorHash)
+import Contract.TextEnvelope as TextEnvelope
+import Contract.Transaction (Language(PlutusV2), TransactionInput)
+import FromData (class FromData, fromData)
+-- import Types.Scripts as Scripts
+import Plutus.Types.CurrencySymbol (CurrencySymbol)
+import RawScripts as RawScripts
+import ToData (class ToData, toData)
+
+-- * Types
+
+-- | 'Ds' is the type which parameterizes the validator script for insertion in
+-- the distributed set.
+newtype Ds = Ds { dsConf ∷ CurrencySymbol }
+
+-- | 'DsDatum' is the datum for the validator script for insertion in the
+-- distributed set.
+newtype DsDatum = DsDatum { dsNext ∷ ByteArray }
+
+-- | 'DsConfDatum' is the datum for the validator script which holds the
+-- configuration of the distributed set on chain.
+newtype DsConfDatum = DsConfDatum
+  { dscKeyPolicy ∷ CurrencySymbol
+  , dscFUELPolicy ∷ CurrencySymbol
+  }
+
+-- | 'DsConfMint' is the type which paramaterizes the minting policy of the NFT
+-- which initializes the distribted set.
+newtype DsConfMint = DsConfMint { dscmTxOutRef ∷ TransactionInput }
+
+-- | 'DsKeyMint' is the type which paramterizes the minting policy of the
+-- tokens which are keys in the distributed set.
+newtype DsKeyMint = DsKeyMint
+  { dskmValidatorHash ∷ ValidatorHash
+  , dskmCurrencySymbol ∷ CurrencySymbol
+  }
+
+-- * Validator / minting policies
+insertValidator ∷ Ds → Contract () Validator
+insertValidator _ds = do
+  -- TODO: apply this to the validator..
+  validatorBytes ← TextEnvelope.textEnvelopeBytes RawScripts.rawInsertValidator
+    PlutusScriptV2
+  let validatorUnapplied = wrap $ wrap $ validatorBytes /\ PlutusV2 ∷ Validator
+  pure validatorUnapplied
+
+-- * ToData / FromData instances.
+-- These should correspond to the on-chain Haskell types.
+
+instance FromData Ds where
+  fromData (Constr n [ a ]) | n == zero = Ds <<< { dsConf: _ } <$> fromData a
+  fromData _ = Nothing
+
+instance ToData Ds where
+  toData (Ds { dsConf }) = Constr zero [ toData dsConf ]
+
+instance FromData DsDatum where
+  fromData (Constr n [ a ]) | n == zero = DsDatum <<< { dsNext: _ } <$> fromData
+    a
+  fromData _ = Nothing
+
+instance ToData DsDatum where
+  toData (DsDatum { dsNext }) = Constr zero [ toData dsNext ]
+
+instance FromData DsConfDatum where
+  fromData (Constr n [ a, b ]) | n == zero =
+    DsConfDatum <$>
+      ({ dscKeyPolicy: _, dscFUELPolicy: _ } <$> fromData a <*> fromData b)
+  fromData _ = Nothing
+
+instance ToData DsConfDatum where
+  toData (DsConfDatum { dscKeyPolicy, dscFUELPolicy }) = Constr zero
+    [ toData dscKeyPolicy, toData dscFUELPolicy ]
+
+instance FromData DsConfMint where
+  fromData (Constr n [ a ])
+    | n == zero = DsConfMint <<< { dscmTxOutRef: _ } <$> fromData a
+  fromData _ = Nothing
+
+instance ToData DsConfMint where
+  toData (DsConfMint { dscmTxOutRef }) = Constr zero [ toData dscmTxOutRef ]
+
+instance FromData DsKeyMint where
+  fromData (Constr n [ a, b ])
+    | n == zero = DsKeyMint <$>
+        ( { dskmValidatorHash: _, dskmCurrencySymbol: _ } <$> fromData a <*>
+            fromData b
+        )
+  fromData _ = Nothing
+
+instance ToData DsKeyMint where
+  toData (DsKeyMint { dskmValidatorHash, dskmCurrencySymbol }) = Constr zero
+    [ toData dskmValidatorHash, toData dskmCurrencySymbol ]
--- ctl/src/InitSidechain.purs
+++ ctl/src/InitSidechain.purs
@@ -1,16 +1,18 @@
-module InitSidechain where
+-- | 'InitSidechain' implements the endpoint for intializing the sidechain.
+module InitSidechain (initSidechain) where
 
 import Contract.Prelude
 
 import BalanceTx.Extra (reattachDatumsInline)
 import Contract.Log (logInfo')
 import Contract.Monad (Contract, liftedE, liftedM)
-import Contract.PlutusData (Datum(..), toData)
+import Contract.PlutusData (Datum(..))
+import Contract.PlutusData as PlutusData
 import Contract.ScriptLookups as Lookups
 import Contract.Scripts (validatorHash)
 import Contract.Transaction (awaitTxConfirmed, balanceAndSignTx, submit)
 import Contract.TxConstraints as Constraints
-import Contract.Utxos (getUtxo)
+import Contract.Utxos as Utxos
 import Data.Array as Array
 import Data.BigInt as BigInt
 import Data.Map as Map
@@ -35,11 +37,20 @@ import UpdateCommitteeHash
 
  Note [Initializing the Committee Hash]
  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
- The intialization step of the committee hash is done in two steps.
+ The intialization step of the committee hash is done as follows.
 
-  (1) Create an NFT which identifies the committee hash
+  (1) Create an NFT which identifies the committee hash / spend the NFT to the
+  script output which contains the committee hsah
 
-  (2) Spend that NFT to a script output which contains the committee hash
+ Note [Initializing the Distributed Set]
+ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ The intialization step of the distributed set is done as follows.
+
+  (1) Create an NFT and pay this to a script which holds 'DsConfDatum' which
+  holds the minting policy of the scripts related to the distributed set.
+
+  (2) Mint node which corresponds to the root of the distributed set
+  i.e., 'DistributedSet.rootNode'
 
  Here, we create a transaction which executes both of these steps with a single
  transaction.
@@ -47,36 +58,55 @@ import UpdateCommitteeHash
 initSidechain ∷ InitSidechainParams → Contract () SidechainParams
 initSidechain (InitSidechainParams isp) = do
   let txIn = isp.initUtxo
-  txOut ← liftedM "Cannot find genesis UTxO" $ getUtxo txIn
+  txOut ← liftedM "initSidechain: cannot find genesis UTxO" $ Utxos.getUtxo txIn
 
+  -- Sidechain parameters
+  -----------------------------------
+  let
+    sc = SidechainParams
+      { chainId: isp.initChainId
+      , genesisHash: isp.initGenesisHash
+      , genesisUtxo: txIn
+      , genesisMint: isp.initMint
+      }
+
+  -- Initializing the committee hash
+  -----------------------------------
   let ichm = InitCommitteeHashMint { icTxOutRef: txIn }
-  nft ← committeeHashAssetClass ichm
-  nftPolicy ← committeeHashPolicy ichm
-  committeeHash ← aggregateKeys $ Array.sort isp.initCommittee
-
+  assetClassCommitteeHash ← committeeHashAssetClass ichm
+  nftCommitteeHashPolicy ← committeeHashPolicy ichm
+  aggregatedKeys ← aggregateKeys $ Array.sort isp.initCommittee
   let
-    val = assetClassValue nft (BigInt.fromInt 1)
-    uch = UpdateCommitteeHash { uchAssetClass: nft }
-
-    ndat = Datum
-      $ toData
-      $ UpdateCommitteeHashDatum
-          { committeeHash }
-
-  updateValidator ← updateCommitteeHashValidator uch
+    committeeHashParam = UpdateCommitteeHash
+      { uchAssetClass: assetClassCommitteeHash }
+    committeeHashDatum = Datum
+      $ PlutusData.toData
+      $ UpdateCommitteeHashDatum { committeeHash: aggregatedKeys }
+    valCommitteeHash = assetClassValue assetClassCommitteeHash (BigInt.fromInt 1)
+  committeeHashValidator ← updateCommitteeHashValidator committeeHashParam
   let
-    valHash = validatorHash updateValidator
+    committeeHashValidatorHash = validatorHash committeeHashValidator
 
+  -- Initializing the distributed set
+  -----------------------------------
+  -- TODO: add distributed set stuff here...
+
+
+  -- Building the transaction
+  -----------------------------------
+  let
     lookups ∷ Lookups.ScriptLookups Void
     lookups =
-      Lookups.mintingPolicy nftPolicy
+      Lookups.mintingPolicy nftCommitteeHashPolicy
         <> Lookups.unspentOutputs (Map.singleton txIn txOut)
-        <> Lookups.validator updateValidator
+        <> Lookups.validator committeeHashValidator
 
     constraints =
       Constraints.mustSpendPubKeyOutput txIn
-        <> Constraints.mustMintValue val
-        <> Constraints.mustPayToScript valHash ndat val
+        <> Constraints.mustMintValue valCommitteeHash
+        <> Constraints.mustPayToScript committeeHashValidatorHash
+          committeeHashDatum
+          valCommitteeHash
 
   ubTx ← liftedE (Lookups.mkUnbalancedTx lookups constraints)
   bsTx ← liftedM "Failed to balance/sign tx"
@@ -86,9 +116,4 @@ initSidechain (InitSidechainParams isp) = do
   awaitTxConfirmed txId
   logInfo' "Inital updateCommitteeHash transaction submitted successfully."
 
-  pure $ SidechainParams
-    { chainId: isp.initChainId
-    , genesisHash: isp.initGenesisHash
-    , genesisUtxo: txIn
-    , genesisMint: isp.initMint
-    }
+  pure sc
