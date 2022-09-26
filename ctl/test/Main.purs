4a318a0e3bd75cc1fddf36c2e3febf359fdfcef6
James Faure <james@mlabs.city>
2022-09-26 23:15:27 +0200

    Added Test.Spec purescript testing framework

--- ctl/test/Main.purs
+++ ctl/test/Main.purs
@@ -1,31 +1,45 @@
 module Test.Main (main) where
 
-import Contract.Prelude
-
 import Contract.Monad (launchAff_)
+import Contract.Prelude (Effect, Maybe(..), Unit, discard, ($), (/\))
 import Contract.Test.Plutip (runPlutipContract)
 import Contract.Wallet (withKeyWallet)
 import Data.BigInt as BigInt
+import Data.Time.Duration (Milliseconds(..))
 import Test.CommitteeCandidateValidator as CommitteeCandidateValidator
 import Test.Config (config)
 import Test.FUELMintingPolicy as FUELMintingPolicy
 import Test.MerkleTree as MerkleTree
+import Test.Spec (describe, it)
+import Test.Spec.Reporter.Console (consoleReporter)
+import Test.Spec.Runner (runSpec')
 import Test.UpdateCommitteeHash as UpdateCommitteeHash
 
--- Note. it is necessary to be running a `plutip-server` somewhere for this
 main ∷ Effect Unit
 main = do
-  -- Run the merkle tree integration tests
-  MerkleTree.test
-
-  -- Run the plutip tests
-  launchAff_ $ do
+  MerkleTree.test -- Merkletree has its own testing framework atm
+  launchAff_ do
     let
       distribute = [ BigInt.fromInt 2_000_000_000, BigInt.fromInt 2_000_000_000 ]
         /\ [ BigInt.fromInt 2_000_000_000 ]
+      testConfig =
+        { slow: Milliseconds 5000.0
+        , timeout: Just (Milliseconds 100_000.0) -- plutip can take 15s+ to terminate
+        , exit: false
+        }
+      plutipTest go = runPlutipContract config distribute
+        \(alice /\ _bob) → withKeyWallet alice go
 
-    runPlutipContract config distribute \(alice /\ _bob) → do
-      withKeyWallet alice $ do
-        CommitteeCandidateValidator.testScenario
-        FUELMintingPolicy.testScenario
-        UpdateCommitteeHash.testScenario
+    runSpec' testConfig [ consoleReporter ] do
+      --    describe "Merkle tree integration tests" do
+      --      it "" MerkleTree.test
+      describe "testScenarios" do
+        it "committeeCandidateValidator" $ plutipTest
+          CommitteeCandidateValidator.testScenario
+        it "fuelMintingPolicy" $ plutipTest FUELMintingPolicy.testScenario
+        it "committeeCandidateValidator" $ plutipTest
+          UpdateCommitteeHash.testScenario
+
+-- it "adds 1 and 1" $ 1 + 1 `shouldEqual` 2
+-- pending "add some test"
+-- pending' "adds 1 and 1" $ 1 + 1 `shouldEqual` 2 -- body will be ignored
