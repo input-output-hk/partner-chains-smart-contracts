5ca6fae480e94cf59723ad2f4c3168343bb5a9f6
Hazem ElMasry <hazem@mlabs.city>
2022-09-27 04:23:15 +0200

    Add failing tests to CommitteeCandidateValidator

--- ctl/test/Main.purs
+++ ctl/test/Main.purs
@@ -2,10 +2,13 @@ module Test.Main (main) where
 
 import Contract.Prelude
 
-import Contract.Monad (launchAff_)
+import Contract.Log (logInfo')
+import Contract.Monad (Contract, launchAff_)
 import Contract.Test.Plutip (runPlutipContract)
 import Contract.Wallet (withKeyWallet)
+import Control.Monad.Error.Class (catchError)
 import Data.BigInt as BigInt
+import Effect.Exception (message)
 import Test.CommitteeCandidateValidator as CommitteeCandidateValidator
 import Test.Config (config)
 import Test.FUELMintingPolicy as FUELMintingPolicy
@@ -19,13 +22,19 @@ main = do
   MerkleTree.test
 
   -- Run the plutip tests
-  launchAff_ $ do
+  launchAff_ do
     let
       distribute = [ BigInt.fromInt 2_000_000_000, BigInt.fromInt 2_000_000_000 ]
         /\ [ BigInt.fromInt 2_000_000_000 ]
 
-    runPlutipContract config distribute \(alice /\ _bob) → do
-      withKeyWallet alice $ do
-        CommitteeCandidateValidator.testScenario
+    runPlutipContract config distribute \(alice /\ bob) → do
+      withKeyWallet alice do
+        CommitteeCandidateValidator.testScenarioSuccess
+        CommitteeCandidateValidator.testScenarioFailure1 # fails
+        CommitteeCandidateValidator.testScenarioFailure2 alice bob # fails
         FUELMintingPolicy.testScenario
         UpdateCommitteeHash.testScenario
+
+-- print nicer failing tests that don't have a stack trace and don't halt the program
+fails ∷ Contract () Unit → Contract () Unit
+fails = flip catchError \e → logInfo' ("Expected failure: " <> message e)
