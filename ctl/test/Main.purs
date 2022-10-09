6ec817f098c11b61c1ebf464492aed43b1dcd902
James Faure <james@mlabs.city>
2022-10-09 18:53:04 +0200

    Mote and parallel plutip server tests

--- ctl/test/Main.purs
+++ ctl/test/Main.purs
@@ -1,41 +1,35 @@
 module Test.Main (main) where
 
-import Contract.Monad (liftedM , Contract , launchAff_)
-import Contract.Prelude (Effect, Maybe(..), Unit, discard, ($), (/\))
-import Contract.Test.Plutip (runPlutipContract)
+import Contract.Monad (liftedM , Contract)
+import Contract.Prelude (Effect, (/\))
+import Contract.Test.Plutip (runPlutipContract , PlutipConfig)
 import Contract.Wallet (withKeyWallet)
 import Data.BigInt as BigInt
-import Data.Time.Duration (Milliseconds(..))
-import Data.Maybe
-import Data.Either
-import Data.Foldable
-import Data.Unfoldable
-import Data.Traversable as Traversable
+import Data.Maybe(Maybe(..))
+import Data.Foldable(sequence_)
 import Data.Const (Const)
+import Data.UInt(UInt , fromInt)
 import Test.CommitteeCandidateValidator as CommitteeCandidateValidator
 import Test.Config (config)
 import Test.FUELMintingPolicy as FUELMintingPolicy
 import Test.MerkleTree as MerkleTree
 import Mote (Mote, plan, group, test)
 import Mote.Plan (Plan, foldPlan)
-import Test.Spec
+import Test.Spec (Spec , describe , it , pending)
 import Test.Spec.Reporter.Console (consoleReporter)
 import Test.Spec.Runner as Runner
 import Test.UpdateCommitteeHash as UpdateCommitteeHash
-import Node.ChildProcess
+import Node.ChildProcess (spawn , stdout)
 import Node.ChildProcess as ChildProcess
-import Node.Stream
-import Node.Encoding as Encoding
-import Node.Buffer as Buffer
-import Prelude
-import Effect.Console
-import Effect.Class
-import Effect.Aff
-import Effect.Ref as Ref
+import Node.Stream(Read , Readable , Stream)
+import Prelude(Unit, Void, bind, const, discard, pure, unit, void, ($), (+), (<$>), (<<<), (<>), (>>=))
+import Effect.Console(log)
+import Effect.Class(liftEffect)
+import Effect.Aff(Aff, Milliseconds(..), launchAff_)
 
 -- tmp fn for testing the testing framework
-failAlways ∷ Contract () Unit
-failAlways = liftedM "fail" (pure Nothing) >>= \_ -> pure unit
+failContract ∷ Contract () Unit
+failContract = liftedM "fail" (pure Nothing) >>= \_ -> pure unit
 
 testConfig :: Runner.Config
 testConfig =
@@ -44,47 +38,69 @@ testConfig =
   , exit: false
   }
 
+-- add number to all ports in config
+incPorts :: UInt -> PlutipConfig -> PlutipConfig
+incPorts n config = ((((config
+  { port = n + config.port })
+  { ogmiosConfig { port = config.ogmiosConfig.port + n } })
+  { ogmiosDatumCacheConfig { port = config.ogmiosDatumCacheConfig.port + n }})
+  { postgresConfig { port = config.postgresConfig.port + n }})
+  { ctlServerConfig = (\cfg -> cfg { port = cfg.port + n }) <$> config.ctlServerConfig }
+
 -- run some contracts in a plutus server seeded with some wallets
-plutipTest :: Contract () Unit -> Aff Unit
-plutipTest go = let
+-- n is the number to increment all plutip config ports
+plutipTest :: Int -> Contract () Unit -> Aff Unit
+plutipTest n go = let
   distribute = [ BigInt.fromInt 2_000_000_000, BigInt.fromInt 2_000_000_000 ]
     /\ [ BigInt.fromInt 2_000_000_000 ]
-  in runPlutipContract config distribute \(alice /\ _bob) → withKeyWallet alice go
+  in runPlutipContract (incPorts (fromInt n) config) distribute \(alice /\ _bob) → withKeyWallet alice go
+
+-- Spec constructed using the `Mote` DSL rather than `Spec`
+oldSpec :: Mote (Const Void) (Aff Unit) Unit
+oldSpec = group "testScenarios" do
+--test "fails" (plutipTest failContract)
+  test "Merkle tree integration tests" $ liftEffect (MerkleTree.test)
+  test "committeeCandidateValidator" $ plutipTest 0 CommitteeCandidateValidator.testScenario
+  test "fuelMintingPolicy" $ plutipTest 1 FUELMintingPolicy.testScenario
+  test "committeeCandidateValidator" $ plutipTest 2 UpdateCommitteeHash.testScenario
+
+-- override stdout to collate results independently
+testRedirectedSpec :: Mote (Const Void) (Aff (Stream (read :: Read))) Unit
+testRedirectedSpec = group "TestTestFramework" $ do
+  test "ok" (liftEffect $ spawnAndWaitForOutput "echo" ["5"] procOptions)
+  test "ko" (liftEffect $ spawnAndWaitForOutput "false" [] procOptions)
 
 main ∷ Effect Unit
 main = do
-  MerkleTree.test -- Merkletree has its own testing framework atm
   launchAff_ do
-    launchPlutipTests
-    Runner.runSpec' testConfig [ consoleReporter ] (interpretMote assertionSpec)
---     runSpec' testConfig [ consoleReporter ] do
---       --    describe "Merkle tree integration tests" $ it "" MerkleTree.test
---       describe "testScenarios" do
--- --      it "committeeCandidateValidator" $ plutipTest CommitteeCandidateValidator.testScenario
---         it "fails" (plutipTest failAlways)
--- --      it "fuelMintingPolicy" $ plutipTest FUELMintingPolicy.testScenario
--- --      it "committeeCandidateValidator" $ plutipTest UpdateCommitteeHash.testScenario
+--  launchPlutipTests
+    Runner.runSpec' testConfig [ consoleReporter ] (interpretMoteIdentity oldSpec)
 
--- Convert a mote to a a Spec
-interpretMote :: Mote (Const Void) (Aff _) Unit -> Spec Unit
-interpretMote = go <<< plan where
+-- Convert a mote to a a Spec, this is left here to clarify whats going on
+interpretMoteIdentity :: Mote (Const Void) (Aff _) Unit -> Spec Unit
+interpretMoteIdentity = go <<< plan where
   go :: Plan (Const Void) (Aff _) -> Spec Unit
   go = foldPlan
     (\{ label , value } -> it label (void value))     -- tests
     (\label -> pending label)                         -- skipped tests
-    (\{ label , value } -> describe label (go value)) -- groups of tests
+    (\{ label , value } -> {-Spec.parallel $-} describe label (go value)) -- groups of tests
     sequence_   -- sequence resulting values from previous handlers (Array i -> r)
 
--- Spec costructed using the `Mote` DSL rather than `Spec`
-assertionSpec :: Mote (Const Void) (Aff _) Unit
-assertionSpec =
---group "Test" $ test "fails" (plutipTest failAlways)
-  group "Test" $ test "ok" (liftEffect $ spawnAndWaitForOutput "echo" ["5"] procOptions)
-
--- standard options for spawning a process with pipe option which redirects stdin,stdout and stderr
-procOptions :: ChildProcess.SpawnOptions
-procOptions = { cwd: Nothing , detached: true , env: Nothing , gid: Nothing
-              , stdio: ChildProcess.pipe , uid: Nothing}
+{-
+-- Convert mote to Spec, this time accumulating output pipes
+interpretMote :: Mote (Const Void) (Aff (Stream (read :: Read))) Unit -> Spec Unit
+interpretMote = go <<< plan where
+  doTest { label , value } = it label $ attempt value >>= case _ of
+    Right s -> do
+      liftEffect $ onDataString s Encoding.UTF8 log
+      pure unit
+    Left e  -> throwError e
+  go :: Plan (Const Void) (Aff _) -> Spec Unit
+  go = foldPlan
+    doTest
+    (\label -> (pending label))          -- skipped tests
+    (\{ label , value } -> describe label (go value))
+    sequence_   -- sequence resulting values from previous handlers (Array i -> r)
 
 -- Spawn plutipTests in their own server instance in parallel and capture outputs
 launchPlutipTests :: Aff Unit
@@ -97,6 +113,12 @@ launchPlutipTests = do
       b = Traversable.traverse_ (_ >>= \s -> onDataString s Encoding.UTF8 log) a
   liftEffect b
   pure unit
+-}
+
+-- standard options for spawning a process with pipe option which redirects stdin,stdout and stderr
+procOptions :: ChildProcess.SpawnOptions
+procOptions = { cwd: Nothing , detached: true , env: Nothing , gid: Nothing
+              , stdio: ChildProcess.pipe , uid: Nothing}
 
 -- make a pipe and return stdout of running process
 spawnAndWaitForOutput :: String -> Array String -> ChildProcess.SpawnOptions -> Effect (Readable ())
