e6d77b03f562d84c6413be101ea19384acaffc1b
jared <>
2022-10-12 00:53:38 -0600

    Improved documentation / cleaned up the regex

--- ctl/src/Options.js
+++ ctl/src/Options.js
@@ -2,12 +2,12 @@
 exports.parsePubKeyAndSignatureImpl = nothing => just => str => {
   // Regexes tend to be a bit unreadable.. As a EBNF grammar, we're matching:
   //   > pubKeyAndSig
-  //   >      -> hexStr [ ':' [hexStr]]
-  // where @hexStr@ is a a sequence of hex digits of even length (the even
-  // length requirement is imposed by 'ByteArray').
-  // i.e., we are parsing a @hexStr@ followed optionally by a colon ':', and
-  // followed optionally by another @hexStr@.
-  const matches = str.match(/^((?:[0-9a-f]{2})*)(?::((?:[0-9a-f]{2})*)?)?$/i);
+  //   >      -> hexStr [ ':' hexStr]
+  // where `hexStr` is a a sequence of hex digits of even length (the even
+  // length requirement is imposed by 'Contract.Prim.ByteArray.hexToByteArray').
+  // i.e., we are parsing a `hexStr` followed optionally by a colon ':', and
+  // followed optionally by another `hexStr`.
+  const matches = str.match(/^((?:[0-9a-f]{2})*)(?::((?:[0-9a-f]{2})*))?$/i);
   if (matches != null) {
     return just(
       { pubKey: matches[1]
--- ctl/src/Options.purs
+++ ctl/src/Options.purs
@@ -300,7 +300,7 @@ options maybeConfig = info (helper <*> optSpec)
                   committeeSignature
                   ( fold
                       [ long "committee-pub-key-and-signature"
-                      , metavar "PUBLIC_KEY[:[SIGNATURE]]"
+                      , metavar "PUBLIC_KEY[:SIGNATURE]"
                       , help
                           "Public key and (optionally) the signature of a committee member seperated by a colon ':'"
                       ]
@@ -389,7 +389,7 @@ committeeSignature = maybeReader $ \str → do
 
 -- | 'parsePubKeyAndSignature' parses (in EBNF)
 --    >  sidechainAddress
---    >         -> hexStr[:[hexStr]]
+--    >         -> hexStr[:hexStr]
 -- where @hexStr@ is a sequence of hex digits i.e, it parses a @hexStr@
 -- public key, followed by an equal sign, followed by an optional signature
 -- @hexStr@.
