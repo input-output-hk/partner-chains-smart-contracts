964a79e2944ecc763e8dd9a2f03dd3f18a5c4935
Hazem ElMasry <hazem@mlabs.city>
2022-10-14 12:37:02 +0200

    More investigation, contract still broken

--- ctl/src/Utils/Crypto.js
+++ ctl/src/Utils/Crypto.js
@@ -1,31 +1,31 @@
 let secp = require("secp256k1");
-let { randomBytes } = require("crypto");
+let crypto = require("crypto");
 
-exports.verifyEcdsaSecp256k1Signature = ecdsa_pub_key => data => ecdsa_sig =>
-  secp.ecdsaVerify(ecdsa_sig, data, ecdsa_pub_key);
+exports.verifyEcdsaSecp256k1Signature = ecdsa_pub_key => data => ecdsa_der_sig =>
+  secp.ecdsaVerify(secp.signatureImport(ecdsa_der_sig), data, ecdsa_pub_key);
 
-exports.sign = ecdsa_priv_key => data =>
-  secp.ecdsaSign(data, ecdsa_priv_key).signature;
+// Signature must be serialized in DER format.
+exports.sign = data => ecdsa_priv_key =>
+  secp.signatureExport(secp.ecdsaSign(data, ecdsa_priv_key).signature);
 
 // rawSerialiseDSIGN for private keys is just hex decoding them, so we need to do nothing here
 exports.generateRandomPrivateKey = () => {
   let priv_key;
   do {
-    priv_key = randomBytes(32);
+    priv_key = crypto.randomBytes(32);
   }
   while (!secp.privateKeyVerify(priv_key));
   return priv_key;
 };
 
 // rawSerialiseDSIGN for public keys format:
-// - 64 bytes, not 65 bytes, and not equal to decompressed pubkey from here (e.g. minus an extra byte)
+// - 33-bit compressed pubkey.
 // - both the cardano version and this version call to secp256k1_ec_pubkey_serialize c function
-// - cardano version: https://github.com/input-output-hk/cardano-base/blob/46cd4c97cff9f1f0a0da976aa9e32bd2899c85ee/cardano-crypto-class/src/Cardano/Crypto/DSIGN/EcdsaSecp256k1.hs#L212-L232
-// - this version: https://github.com/cryptocoinjs/secp256k1-node/blob/024fbdad3fb64499e8db8b35246c2f0a36afa8c8/src/secp256k1.cc#L139-L147
-//                 https://github.com/cryptocoinjs/secp256k1-node/blob/024fbdad3fb64499e8db8b35246c2f0a36afa8c8/src/secp256k1.cc#L18-L26
-// - cardano version claims they only pass the compressed flag to the c function
-//   https://github.com/input-output-hk/cardano-base/blob/46cd4c97cff9f1f0a0da976aa9e32bd2899c85ee/cardano-crypto-class/src/Cardano/Crypto/SECP256K1/C.hs#L163
-// - I'm not sure how they end up getting their serialized values, this needs more looking into.
-// - XXX: in terms of compatibility with the Haskell onchain code, the output here is broken.
+// - neither deals directly with the internal state of the pubkeys, especially not onchain
+// see:
+// - https://github.com/cardano-foundation/CIPs/blob/c5bdd66fe49c19c341499f86cebaa2eef9e90b74/CIP-0049/README.md#specification
+// - https://github.com/input-output-hk/cardano-base/blob/737d0c50d10db63ee55f9a49c66da50573088818/cardano-crypto-class/src/Cardano/Crypto/DSIGN/EcdsaSecp256k1.hs#L225
+// - https://github.com/input-output-hk/cardano-base/blob/737d0c50d10db63ee55f9a49c66da50573088818/cardano-crypto-class/src/Cardano/Crypto/SECP256K1/C.hs#L157-L164
+// - https://github.com/input-output-hk/cardano-base/pull/289
 exports.toPubKeyUnsafe = ecdsa_priv_key =>
-  secp.publicKeyCreate(ecdsa_priv_key, /*compressed =*/ false);
+  secp.publicKeyCreate(ecdsa_priv_key, /*compressed =*/ true);
--- ctl/src/Utils/Crypto.purs
+++ ctl/src/Utils/Crypto.purs
@@ -15,26 +15,33 @@ import Contract.Prelude
 import Contract.Monad (Contract)
 import Types.ByteArray (ByteArray)
 
--- | Invariant: ∀ x : PublicKey. length x = 33 ∨ length x = 65
+-- | Invariant: ∀ x : PublicKey. length x = 33
+-- | Format: Compressed & Serialized as per secp256k1 implementation
+-- make sure to check the leading byte for valid key format
 type PublicKey = ByteArray
 
 -- | Invariant: ∀ x : PrivateKey. length x = 32
+-- | Format: raw bytes
 type PrivateKey = ByteArray
 
 -- | Invariant: ∀ x : Message. length x = 32
+-- | Format: raw bytes
 type Message = ByteArray
 
--- | Invariant: ∀ x : Signature. length x = 64
+-- | Invariant: ∀ x : Signature. length x = 71
+-- | Format: DER encoding
 type Signature = ByteArray
+
+-- TODO: newtype checks the type aliases above
 
 foreign import generateRandomPrivateKey ∷ Effect PrivateKey
 foreign import toPubKeyUnsafe ∷ PrivateKey → PublicKey
-foreign import sign ∷ PrivateKey → Message → Signature
+foreign import sign ∷ Message → PrivateKey → Signature
 foreign import verifyEcdsaSecp256k1Signature ∷
   PublicKey → Message → Signature → Boolean
 
-multiSign ∷ Array PublicKey → Message → Array Signature
-multiSign xkeys msg = map (flip sign msg) xkeys
+multiSign ∷ Array PrivateKey → Message → Array Signature
+multiSign xkeys msg = map (sign msg) xkeys
 
 generatePrivKey ∷ Contract () ByteArray
 generatePrivKey = liftEffect generateRandomPrivateKey
