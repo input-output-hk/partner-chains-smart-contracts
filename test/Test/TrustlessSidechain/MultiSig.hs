a42584b6c8d87b6b18428864c991cf2368c85608
Hazem ElMasry <hazem@mlabs.city>
2022-10-18 18:01:06 +0200

    Update multisig tests to pin down their behavior
    Now we can pass properly formatted (DER) pubkeys and signatures
    This closes #284

--- test/Test/TrustlessSidechain/MultiSig.hs
+++ test/Test/TrustlessSidechain/MultiSig.hs
@@ -13,21 +13,31 @@ test = unitTests
 unitTests :: TestTree
 unitTests =
   let vmbs = U.verifyMulti @BuiltinByteString @BuiltinByteString
+      -- XXX: The following is a lie. Investigate why.
+      --
+      -- >>> conv "hello" == "hello"
+      -- True
+      --
+      -- Only explicitly converted bytestrings work with verifyMultisig.
       conv = toBuiltin @ByteString
+      -- blake2b_256 "msg"
+      msg = conv "\150\167\237\136a\219\n\188\NULoG?\158dhxu\243\217\223\142r:\218\233\245:\STX\178\174\195x"
+      -- skey1 = genKeyDSIGN . mkSeedFromBytes $ blake2b_256 "321"
+      -- skey2 = ... "123"
+      -- rawSerialiseVerKeyDSIGN $ deriveVerKeyDSIGN skeyN
+      key1 = conv "\ETX\US\205\137A\137\US\164\158\SO\ETB\DLE\168<U/_\165(\169\226y\NUL|Mp\rQ\ENQ\239\158\195\250"
+      key2 = conv "\ETX}\241\\\253\\\164\156\RS\209\176M#\242yy\132\SOG\190\193\254\211\194%\SUB\223M*\STXb\237H"
+      -- rawSerialiseSigDSIGN $ signDSIGN () msg skeyN
+      sig1 = conv "\148{qy5\CAN\f\154Ni\144\&2I\171\SUB\v\148\229I\SOH\140\163\209$\t\139\242H\139\204\147\DC4x\238%/\178>\193[\190\184\229\216\&7\214\235\153\225d\216t \132\138\207\246\&6\154\209\133/tF"
+      sig2 = conv "\190\160N\239)\238~j\STX\v\CAN\CAN\r\226q\\8ts\DC3!g\233dm\ne\148\205z'\aM\218\218\226\205\209\&8\153\&9_\161.\b91v\211d\ETXY\254\220fP\US\216%\137\201\249S\184"
    in testGroup
         "MultiSig"
-        [ testCase "unsorted sigs" $
-            let -- blake2b_256 "msg"
-                msg = conv "\150\167\237\136a\219\n\188\NULoG?\158dhxu\243\217\223\142r:\218\233\245:\STX\178\174\195x"
-                -- skey1 = genKeyDSIGN . mkSeedFromBytes $ blake2b_256 "123"
-                -- skey2 = ... "321"
-                -- rawSerialiseVerKeyDSIGN $ deriveVerKeyDSIGN skeyN
-                key1 = conv "H\237b\STX*M\223\SUB%\194\211\254\193\190G\SO\132yy\242#M\176\209\RS\156\164\\\253\\\241}i\225\206\US\202\&3\133\132\206S\173\190@7X\250\SOx\223hS\210{\135\189wd\149\ESC:\143\202"
-                key2 = conv "\250\195\158\239\ENQQ\rpM|\NULy\226\169(\165_/U<\168\DLE\ETB\SO\158\164\US\137A\137\205\US\ETX\177(]\US\129\157\DC2\DLEB>\228\185\STX!\231\151\&5\DLE00R\232\135\\/\244\216`<\200\FS"
-                -- rawSerialiseSigDSIGN $ signDSIGN () skeyN msg
-                sig1 = conv "\a'z\205\148e\nmd\233g!\DC3st8\\q\226\r\CAN\CAN\v\STXj~\238)\239N\160\190\184S\249\201\137%\216\USPf\220\254Y\ETXd\211v19\b.\161_9\153\&8\209\205\226\218\218M"
-                sig2 = conv "\DC4\147\204\139H\242\139\t$\209\163\140\SOHI\229\148\v\SUB\171I2\144iN\154\f\CAN5yq{\148Ft/\133\209\154\&6\246\207\138\132 t\216d\225\153\235\214\&7\216\229\184\190[\193>\178/%\238x"
-             in U.verifyMultisig [key1, key2] 1 msg [sig2, sig1] @?= True
+        [ testCase "key2 > key1" $
+            key2 > key1 @?= True
+        , testCase "unsorted keys" $
+            U.verifyMultisig [key2, key1] 1 msg [sig1, sig2] @?= False
+        , testCase "sorted keys, unsorted sigs" $
+            U.verifyMultisig [key1, key2] 1 msg [sig2, sig1] @?= True
         , testCase "0 threshold" $
             vmbs (==) 0 [] [] @?= True
         , testCase "not eq" $
