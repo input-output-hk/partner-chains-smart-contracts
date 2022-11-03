d2a3ef0316c137682b7ee511a860b63e3b5b5d8e
Szabo Gergely <gege251@mailbox.org>
2022-11-03 17:32:48 +0100

    Add support for aarc64

--- flake.nix
+++ flake.nix
@@ -42,7 +42,12 @@
         };
       };
 
-      supportedSystems = [ "x86_64-linux" "x86_64-darwin" ];
+      supportedSystems = [
+        "x86_64-linux"
+        "x86_64-darwin"
+        "aarch64-linux"
+        "aarch64-darwin"
+      ];
 
       perSystem = nixpkgs.lib.genAttrs supportedSystems;
 
