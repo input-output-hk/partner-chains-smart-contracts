d1f39ab93661adf8dd84622b175f1b73bc0747ad
jared <>
2022-11-09 14:58:45 -0700

    Improved documentation

--- ctl/src/SidechainParams.purs
+++ ctl/src/SidechainParams.purs
@@ -66,7 +66,7 @@ newtype InitSidechainParams = InitSidechainParams
     initUtxo ∷ TransactionInput
   , -- | 'initCommittee' is the initial committee of the sidechain
     initCommittee ∷ Array PubKey
-  , -- | 'initSidechainEpoch' is the initial sidechain epoch of the sidechain
+  , -- 'initSidechainEpoch' is the initial sidechain epoch of the first committee
     initSidechainEpoch ∷ BigInt
   , initMint ∷ Maybe TransactionInput
   , initThresholdNumerator ∷ BigInt
