5354d7dcf8b57f7ce3d91ec6c51d3682075e6369
Szabo Gergely <gege251@mailbox.org>
2022-11-17 14:22:56 +0100

    Integrate bundle script with our flake

--- nix/ctl-bundle-cli.nix
+++ nix/ctl-bundle-cli.nix
@@ -1,22 +1,32 @@
-{ pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/d1c3fea7ecbed758168787fe4e4a3157e52bc808.tar.gz") { }
+{ pkgs ? import
+    (fetchTarball {
+      url = "https://github.com/NixOS/nixpkgs/archive/d1c3fea7ecbed758168787fe4e4a3157e52bc808.tar.gz";
+      sha256 = "0ykm15a690v8lcqf2j899za3j6hak1rm3xixdxsx33nz7n3swsyy";
+    })
+    { }
+, easy-ps ? import
+    (fetchTarball {
+      url = "https://github.com/justinwoo/easy-purescript-nix/archive/ddd2ded8d37ab5d3013f353ca3b6ee05eb23d5c0.tar.gz";
+      sha256 = "1i7zqda52npklj3d7pq80zw5rfjjzdqpl5bdrsp6vchg5frgj6ky";
+    })
+    { inherit pkgs; }
+, purs ? easy-ps.purs-0_14_5
+, nodejs ? pkgs.nodejs-14_x
+, spagoPkgs ? import ../ctl/spago-packages.nix { inherit pkgs; }
 }:
-let
-  spagoPkgs = import ../spago-packages.nix { inherit pkgs; };
-in
 pkgs.stdenv.mkDerivation rec {
-  pname = "ctl-scripts";
+  pname = "ctl-bundle-cli";
   version = "0.1.0";
 
-  src = ./..;
+  src = ../ctl;
 
   # based on https://discourse.purescript.org/t/spago2nix-any-complete-example-to-look-at/2532/2
   buildInputs = [
-    pkgs.spago
-    pkgs.purescript
-    pkgs.nodejs
+    purs
+    nodejs
+    easy-ps.spago
     spagoPkgs.installSpagoStyle
     spagoPkgs.buildSpagoStyle
-    spagoPkgs.buildFromNixStore
   ];
 
   unpackPhase = ''
