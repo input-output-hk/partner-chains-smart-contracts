796ba7199b50f0676133694632842ebbace95cb3
jared <>
2023-02-01 01:57:39 -0700

    Added other tests for init + update committee hash

--- bench/Main.hs
+++ bench/Main.hs
@@ -8,6 +8,8 @@ import Bench qualified
 import Control.Monad qualified as Monad
 
 import Cases.FUELMintingPolicy qualified as FUELMintingPolicy
+import Cases.InitSidechain qualified as InitSidechain
+import Cases.UpdateCommitteeHash qualified as UpdateCommitteeHash
 
 main :: IO ()
 main = do
@@ -17,6 +19,11 @@ main = do
   --    for querying your utxos)
   --    - You have called `spago build` in `ctl/`
   --    - You have a symlink `ln -s ./ctl/output/ output/`
+  --
+  -- Then, to use, run
+  -- ```
+  -- cabal bench
+  -- ```
   Monad.void $ do
     db <- Bench.freshBenchResults "." "FUELMintingBenchmarks.db"
     let cfg =
@@ -26,3 +33,23 @@ main = do
             , bcfgTestNetMagic = 2
             }
     Bench.runBench cfg FUELMintingPolicy.fuelMintingBench
+
+  Monad.void $ do
+    db <- Bench.freshBenchResults "." "UpdateCommitteeHash.db"
+    let cfg =
+          BenchConfig
+            { bcfgBenchResults = db
+            , bcfgSigningKeyFilePath = "./payment.skey"
+            , bcfgTestNetMagic = 2
+            }
+    Bench.runBench cfg UpdateCommitteeHash.updateCommitteeHashBench
+
+  Monad.void $ do
+    db <- Bench.freshBenchResults "." "InitSidechain.db"
+    let cfg =
+          BenchConfig
+            { bcfgBenchResults = db
+            , bcfgSigningKeyFilePath = "./payment.skey"
+            , bcfgTestNetMagic = 2
+            }
+    Bench.runBench cfg InitSidechain.initSidechainBench
