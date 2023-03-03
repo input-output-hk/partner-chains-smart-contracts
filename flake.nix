5715e92d7f715d518ad8077379ad10c798f8b752
Szabo Gergely <gege251@mailbox.org>
2023-03-03 13:54:38 +0100

    Remove Plutonomicon and MLabs caches from our flake

--- flake.nix
+++ flake.nix
@@ -2,8 +2,8 @@
   description = "trustless-sidechain";
 
   nixConfig = {
-    extra-substituters = [ "https://cache.iog.io" "https://public-plutonomicon.cachix.org" "https://mlabs.cachix.org" ];
-    extra-trusted-public-keys = [ "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ=" "public-plutonomicon.cachix.org-1:3AKJMhCLn32gri1drGuaZmFrmnue+KkKrhhubQk/CWc=" ];
+    extra-substituters = [ "https://cache.iog.io" ];
+    extra-trusted-public-keys = [ "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ=" ];
   };
 
   inputs = {
