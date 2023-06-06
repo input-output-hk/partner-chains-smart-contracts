97c93bce243203fc6df5d21b100199b52931d8b8
Koz Ross <koz.ross@retro-freedom.nz>
2023-06-07 11:43:48 +1200

    Measure all validators and MPs

--- onchain/test/script-size/Main.hs
+++ onchain/test/script-size/Main.hs
@@ -14,7 +14,37 @@ import Test.Tasty (defaultMain, testGroup)
 import TrustlessSidechain.CandidatePermissionMintingPolicy (
   mkCandidatePermissionMintingPolicy,
  )
-import TrustlessSidechain.Types (CandidatePermissionMint)
+import TrustlessSidechain.CheckpointValidator (
+  mkCheckpointValidator,
+ )
+import TrustlessSidechain.CommitteeCandidateValidator (
+  mkCommitteeCandidateValidator,
+ )
+import TrustlessSidechain.FUELMintingPolicy qualified as FUEL
+import TrustlessSidechain.MerkleRootTokenMintingPolicy as MerkleRoot
+import TrustlessSidechain.PoCInlineDatum (
+  mkPoCInlineDatumValidator,
+ )
+import TrustlessSidechain.Types (
+  BlockProducerRegistration,
+  CandidatePermissionMint,
+  CheckpointDatum,
+  CheckpointParameter,
+  CheckpointRedeemer,
+  FUELMint,
+  FUELRedeemer,
+  SidechainParams,
+  SignedMerkleRoot,
+  SignedMerkleRootMint,
+  UpdateCommitteeHash,
+  UpdateCommitteeHashDatum,
+  UpdateCommitteeHashRedeemer,
+ )
+import TrustlessSidechain.UpdateCommitteeHash (
+  InitCommitteeHashMint,
+  mkCommitteeHashPolicy,
+  mkUpdateCommitteeHashValidator,
+ )
 import TrustlessSidechain.Utils (verifyMultisig)
 import Prelude qualified
 
@@ -29,14 +59,114 @@ main =
         "mkCandidatePermissionMintingPolicy"
         mkCPMPCode
         155
+    , fitsInto
+        "mkCheckpointValidator"
+        mkCVCode
+        1_866
+    , fitsInto
+        "mkCommitteeCandidateValidator"
+        mkCCVCode
+        201
+    , fitsInto
+        "mkMintingPolicy (FUEL)"
+        mkMPFuelCode
+        1_049
+    , fitsInto
+        "mkMintingPolicy (MerkleRoot)"
+        mkMPMerkleRootCode
+        1_548
+    , fitsInto
+        "mkPoCInlineDatumValidator"
+        mkPIDVCode
+        379
+    , fitsInto
+        "mkUpdateCommitteeHashValidator"
+        mkUPCVCode
+        1_869
+    , fitsInto
+        "mkCommitteeHashPolicy"
+        mkCommitteeHashPolicyCode
+        458
     ]
 
 -- Helpers
 
 newVerify ::
-  CompiledCode ([BuiltinByteString] -> Integer -> BuiltinByteString -> [BuiltinByteString] -> Bool)
+  CompiledCode
+    ( [BuiltinByteString] ->
+      Integer ->
+      BuiltinByteString ->
+      [BuiltinByteString] ->
+      Bool
+    )
 newVerify = $$(compile [||verifyMultisig||])
 
 mkCPMPCode ::
   CompiledCode (CandidatePermissionMint -> () -> ScriptContext -> Bool)
 mkCPMPCode = $$(compile [||mkCandidatePermissionMintingPolicy||])
+
+mkCVCode ::
+  CompiledCode
+    ( CheckpointParameter ->
+      CheckpointDatum ->
+      CheckpointRedeemer ->
+      ScriptContext ->
+      Bool
+    )
+mkCVCode = $$(compile [||mkCheckpointValidator||])
+
+mkCCVCode ::
+  CompiledCode
+    ( SidechainParams ->
+      BlockProducerRegistration ->
+      () ->
+      ScriptContext ->
+      Bool
+    )
+mkCCVCode = $$(compile [||mkCommitteeCandidateValidator||])
+
+mkMPFuelCode ::
+  CompiledCode
+    ( FUELMint ->
+      FUELRedeemer ->
+      ScriptContext ->
+      Bool
+    )
+mkMPFuelCode = $$(compile [||FUEL.mkMintingPolicy||])
+
+mkMPMerkleRootCode ::
+  CompiledCode
+    ( SignedMerkleRootMint ->
+      SignedMerkleRoot ->
+      ScriptContext ->
+      Bool
+    )
+mkMPMerkleRootCode = $$(compile [||MerkleRoot.mkMintingPolicy||])
+
+mkPIDVCode ::
+  CompiledCode
+    ( Integer ->
+      Integer ->
+      ScriptContext ->
+      Bool
+    )
+mkPIDVCode = $$(compile [||mkPoCInlineDatumValidator||])
+
+mkUPCVCode ::
+  CompiledCode
+    ( UpdateCommitteeHash ->
+      UpdateCommitteeHashDatum ->
+      UpdateCommitteeHashRedeemer ->
+      ScriptContext ->
+      Bool
+    )
+mkUPCVCode = $$(compile [||mkUpdateCommitteeHashValidator||])
+
+mkCommitteeHashPolicyCode ::
+  CompiledCode
+    ( InitCommitteeHashMint ->
+      () ->
+      ScriptContext ->
+      Bool
+    )
+mkCommitteeHashPolicyCode = $$(compile [||mkCommitteeHashPolicy||])
