a882d853e019c9513cd6eff1cfe53c6ae686f61f
jared <>
2023-07-06 16:11:25 -0600

    Removed redundant variables

--- offchain/src/TrustlessSidechain/Utils/Schnorr.js
+++ offchain/src/TrustlessSidechain/Utils/Schnorr.js
@@ -6,26 +6,22 @@ let { sha256 } = require("@noble/hashes/sha256");
 // Mostly straightforward wrappers of this.
 
 exports.js_randomPrivateKey = () => {
-  const privateKey = secp.utils.randomPrivateKey();
-  return privateKey;
+  return secp.utils.randomPrivateKey();
 };
 
 exports.js_getPublicKey = (privateKey) => {
   // See: https://github.com/paulmillr/noble-secp256k1/tree/1.7.1#schnorrgetpublickeyprivatekey
-  const publicKey = secp.schnorr.getPublicKey(privateKey);
-  return publicKey;
+  return secp.schnorr.getPublicKey(privateKey);
 };
 
 exports.js_sign = (msg) => (priv) =>  {
   // See: https://github.com/paulmillr/noble-secp256k1/tree/1.7.1#schnorrsignmessage-privatekey
   secp.utils.sha256Sync = (...msgs) => sha256(secp.utils.concatBytes(...msgs));
 
-  const signature = secp.schnorr.signSync(msg, priv);
-  return signature;
+  return secp.schnorr.signSync(msg, priv);
 };
 
 exports.js_verify = (signature) => (message) =>  (publicKey) => {
   // See: https://github.com/paulmillr/noble-secp256k1/tree/1.7.1#schnorrverifysignature-message-publickey
-  const isValid = secp.schnorr.verify(signature, message, publicKey);
-  return isValid;
+  return secp.schnorr.verify(signature, message, publicKey);
 };
