25515328a8ffd666fc34a3998c3ce2742566bd30
Jan Stolarek <jan.stolarek@mailbox.org>
2023-10-25 14:56:10 +0200

    Increase funds in tests wallets
    Hopefully, this stops random test failures

--- offchain/test/Test/Versioning.purs
+++ offchain/test/Test/Versioning.purs
@@ -52,6 +52,8 @@ testScenarioSuccess =
         , BigInt.fromInt 50_000_000
         , BigInt.fromInt 50_000_000
         , BigInt.fromInt 40_000_000
+        , BigInt.fromInt 40_000_000
+        , BigInt.fromInt 40_000_000
         ]
     $ \alice → Wallet.withKeyWallet alice do
         pkh ← getOwnPaymentPubKeyHash
