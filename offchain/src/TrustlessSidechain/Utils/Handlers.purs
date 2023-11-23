b7777f2e2d65f4e78581279eb83d7e2d782a6570
Jan Stolarek <jan.stolarek@mailbox.org>
2023-11-23 16:06:09 +0100

    Hacking session with Michał

--- offchain/src/TrustlessSidechain/Utils/Handlers.purs
+++ offchain/src/TrustlessSidechain/Utils/Handlers.purs
@@ -22,7 +22,44 @@ runAffEffects ∷ Run (AFF + ()) Unit → Contract Unit
 runAffEffects =
   Run.interpret (Run.case_ # Run.on (Proxy ∷ Proxy "aff") Aff.liftAff)
 
+data ContractEff a b = EmbedContract (Contract a) b
+
+derive instance functorLogF ∷ Functor (ContractEff a)
+
+type CONTRACT a r = (contract ∷ ContractEff a | r)
+
+_contract = Proxy ∷ Proxy "contract"
+
+embedContract ∷ ∀ r a. Contract a → Run (CONTRACT a + r) Unit
+embedContract f = Run.lift _contract (EmbedContract f unit)
+
+handleContract ∷ ∀ r a. ContractEff a ~> Run (AFF + r)
+handleContract = case _ of
+  EmbedContract f next → do
+    -- Run.lift _contract f
+    pure next
+
+runContract ∷ ∀ r a. Run (CONTRACT a + r) ~> Run (AFF + r)
+runContract = Run.interpret (Run.on _contract handleContract Run.send)
+
 {-
+handleLog ∷ ∀ r. LogEff ~> Run (AFF + r)
+handleLog = case _ of
+  LogMsg msg next → do
+    Run.liftAff $ appendTextFile UTF8 "./contractlog.json"
+      (jsonFormatter msg <> "\n")
+    pure next
+
+-}
+
+{-
+runInternalErrorEffects ∷ forall r a. Show a => Run (EXCEPT InternalError + r) a -> Run (CONTRACT + r) a
+runInternalErrorEffects = ?foo
+
+h :: Run (? + AFF + r) ~> Run (AFF + r)
+h = do
+  x <- liftAff (runContract env getOwnPkh)
+
 runSidechainEffects :: ∀ r. Run SidechainEffects Unit -> Contract Unit
 runSidechainEffects f = do
   liftContractE (runExcept f) >>= (liftContractE >>= runExcept)
