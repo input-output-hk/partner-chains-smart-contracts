0c3fcec727bb206c818c10f4c38e4f09ff8ce89d
Jan Stolarek <jan.stolarek@mailbox.org>
2023-11-28 09:43:22 +0100

    Implement CONTRACT effect handler

--- offchain/src/TrustlessSidechain/Utils/Handlers.purs
+++ offchain/src/TrustlessSidechain/Utils/Handlers.purs
@@ -2,11 +2,12 @@ module TrustlessSidechain.Utils.Handlers where
 
 import Contract.Prelude
 
-import Contract.Monad (Contract)
-import Effect.Aff.Class as Aff
+import Contract.Monad (Contract, ContractParams)
+import Contract.Monad as Contract
 import Run (AFF, Run)
 import Run as Run
-import Run.Except (EXCEPT, runExcept)
+import Run.Except (EXCEPT)
+import Run.Except as Run
 import TrustlessSidechain.Utils.Error (InternalError, OffchainError)
 import Type.Proxy (Proxy(Proxy))
 import Type.Row (type (+))
@@ -14,53 +15,52 @@ import Type.Row (type (+))
 type SidechainEffects =
   ( EXCEPT OffchainError
       + EXCEPT InternalError
+      + CONTRACT Unit
       + AFF
       + ()
   )
 
-runAffEffects ∷ Run (AFF + ()) Unit → Contract Unit
-runAffEffects =
-  Run.interpret (Run.case_ # Run.on (Proxy ∷ Proxy "aff") Aff.liftAff)
+--
 
-data ContractEff a b = EmbedContract (Contract a) b
+{-
+runSidechainEffects :: ContractParams -> Run (CONTRACT Unit + AFF + ()) ~> Aff
+runSidechainEffects params f =
+  Run.runBaseAff (runContract params f)
 
-derive instance functorLogF ∷ Functor (ContractEff a)
+mapExcept :: forall a b r. (a -> b) -> Run (EXCEPT a + r) ~> Run (EXCEPT b + r)
+mapExcept f r1 = do
+  x <- Run.runExcept r1
+  case x of
+    Right x' -> pure x'
+    Left y' -> ?g --throw (f y')
+-}
+
+--liftContractE (runExcept f) >>= (liftContractE >>= runExcept)
+
+-- JSTOLAREK: redundant, we want to interpret to Aff
+-- runAffEffects ∷ Run (AFF + ()) Unit → Contract Unit
+-- runAffEffects =
+--   Run.interpret (Run.case_ # Run.on (Proxy ∷ Proxy "aff") Aff.liftAff)
+
+data ContractEff a b = EmbedContract (Contract a) (a → b)
+
+derive instance Functor (ContractEff a)
 
 type CONTRACT a r = (contract ∷ ContractEff a | r)
 
 _contract = Proxy ∷ Proxy "contract"
+_aff = Proxy ∷ Proxy "aff"
 
-embedContract ∷ ∀ r a. Contract a → Run (CONTRACT a + r) Unit
-embedContract f = Run.lift _contract (EmbedContract f unit)
+embedContract ∷ ∀ r a. Contract a → Run (CONTRACT a + r) a
+embedContract f = Run.lift _contract (EmbedContract f identity)
 
-handleContract ∷ ∀ r a. ContractEff a ~> Run (AFF + r)
-handleContract = case _ of
-  EmbedContract f next → do
-    -- Run.lift _contract f
-    pure next
+handleContract ∷ ∀ r a. ContractParams → ContractEff a ~> Run (AFF + r)
+handleContract params = case _ of
+  EmbedContract f k → do
+    res ← Run.lift _aff (Contract.runContract params f)
+    pure (k res)
 
-runContract ∷ ∀ r a. Run (CONTRACT a + r) ~> Run (AFF + r)
-runContract = Run.interpret (Run.on _contract handleContract Run.send)
-
-{-
-handleLog ∷ ∀ r. LogEff ~> Run (AFF + r)
-handleLog = case _ of
-  LogMsg msg next → do
-    Run.liftAff $ appendTextFile UTF8 "./contractlog.json"
-      (jsonFormatter msg <> "\n")
-    pure next
-
--}
-
-{-
-runInternalErrorEffects ∷ forall r a. Show a => Run (EXCEPT InternalError + r) a -> Run (CONTRACT + r) a
-runInternalErrorEffects = ?foo
-
-h :: Run (? + AFF + r) ~> Run (AFF + r)
-h = do
-  x <- liftAff (runContract env getOwnPkh)
-
-runSidechainEffects :: ∀ r. Run SidechainEffects Unit -> Contract Unit
-runSidechainEffects f = do
-  liftContractE (runExcept f) >>= (liftContractE >>= runExcept)
--}
+runContract ∷
+  ∀ r a. ContractParams → Run (CONTRACT a + AFF + r) ~> Run (AFF + r)
+runContract params =
+  Run.interpret (Run.on _contract (handleContract params) Run.send)
