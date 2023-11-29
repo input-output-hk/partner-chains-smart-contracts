f162af14bd635663fb9b663683e483b1abf31cea
Jan Stolarek <jan.stolarek@mailbox.org>
2023-11-29 09:32:15 +0100

    Move Contract effect to a dedicated module

---
+++ offchain/src/TrustlessSidechain/Effects/Contract.purs
@@ -0,0 +1,48 @@
+-- | Effects and handlers for interacting with CTL's Contract monad
+module TrustlessSidechain.Effects.Contract
+  ( ContractEff
+  , CONTRACT
+  , embedContract
+  , runContract
+  ) where
+
+import Contract.Prelude
+
+import Contract.Monad (Contract, ContractParams)
+import Contract.Monad as Contract
+import Run (AFF, Run)
+import Run as Run
+import Type.Proxy (Proxy(Proxy))
+import Type.Row (type (+))
+
+-- | Representation of a `Contract` effect.
+data ContractEff a b =
+  -- | Embedded `Contract` computations and a continuation that makes use of the
+  -- | result.
+  EmbedContract (Contract a) (a → b)
+
+derive instance Functor (ContractEff a)
+
+-- | Type alias for convenient use with `+` type operator.
+type CONTRACT a r = (contract ∷ ContractEff a | r)
+
+_contract ∷ Proxy "contract"
+_contract = Proxy
+
+-- | Embed `Contract` action inside `Run` monad.
+embedContract ∷ ∀ r a. Contract a → Run (CONTRACT a + r) a
+embedContract f = Run.lift _contract (EmbedContract f identity)
+
+-- | A helper functions used by the actual handler.  Runs `Contract` actions by
+-- | embedding them inside `AFF` effects.
+handleContract ∷ ∀ r a. ContractParams → ContractEff a ~> Run (AFF + r)
+handleContract params = case _ of
+  EmbedContract f k → do
+    res ← Run.lift (Proxy ∷ Proxy "aff") (Contract.runContract params f)
+    pure (k res)
+
+-- | Handle `CONTRACT` effect by interpreting it in terms of `AFF` effect.
+runContract ∷
+  ∀ r a. ContractParams → Run (CONTRACT a + AFF + r) ~> Run (AFF + r)
+runContract params =
+  Run.interpret (Run.on _contract (handleContract params) Run.send)
--- offchain/src/TrustlessSidechain/Utils/Handlers.purs
+++ offchain/src/TrustlessSidechain/Utils/Handlers.purs
@@ -2,18 +2,17 @@ module TrustlessSidechain.Utils.Handlers where
 
 import Contract.Prelude
 
-import Contract.Monad (Contract, ContractParams)
-import Contract.Monad as Contract
+import Contract.Monad (ContractParams)
 import Run (AFF, Run)
 import Run as Run
 import Run.Except (EXCEPT)
+import TrustlessSidechain.Effects.Contract (CONTRACT, runContract)
 import TrustlessSidechain.Utils.Error
   ( InternalError
   , OffchainError
   , runInternalError
   , runShowError
   )
-import Type.Proxy (Proxy(Proxy))
 import Type.Row (type (+))
 
 type SidechainEffects =
@@ -30,41 +29,3 @@ runSidechainEffects params =
     >>> runShowError
     >>> runContract params
     >>> Run.runBaseAff
-
-{-
-foo :: forall a r. Proxy a -> Run r ~> Run (EXCEPT a + r)
-foo _ = Run.lift (Proxy :: Proxy "except")
-
-mapExcept :: forall a b r. (a -> b) -> Run (EXCEPT a + r) ~> Run (EXCEPT b + r)
-mapExcept f r1 = do
-  res <- Run.expand (Except.runExcept r1)
-  Except.rethrow (f `lmap` res)
--}
-
--- JSTOLAREK: redundant, we want to interpret to Aff
--- runAffEffects ∷ Run (AFF + ()) Unit → Contract Unit
--- runAffEffects =
---   Run.interpret (Run.case_ # Run.on (Proxy ∷ Proxy "aff") Aff.liftAff)
-
-data ContractEff a b = EmbedContract (Contract a) (a → b)
-
-derive instance Functor (ContractEff a)
-
-type CONTRACT a r = (contract ∷ ContractEff a | r)
-
-_contract = Proxy ∷ Proxy "contract"
-_aff = Proxy ∷ Proxy "aff"
-
-embedContract ∷ ∀ r a. Contract a → Run (CONTRACT a + r) a
-embedContract f = Run.lift _contract (EmbedContract f identity)
-
-handleContract ∷ ∀ r a. ContractParams → ContractEff a ~> Run (AFF + r)
-handleContract params = case _ of
-  EmbedContract f k → do
-    res ← Run.lift _aff (Contract.runContract params f)
-    pure (k res)
-
-runContract ∷
-  ∀ r a. ContractParams → Run (CONTRACT a + AFF + r) ~> Run (AFF + r)
-runContract params =
-  Run.interpret (Run.on _contract (handleContract params) Run.send)
