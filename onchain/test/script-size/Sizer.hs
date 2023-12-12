8fa895bb81714074460b367cbeda0886c36a66ac
Jan Stolarek <jan.stolarek@mailbox.org>
2023-12-12 15:12:19 +0100

    Optimize scripts before testing their size

--- onchain/test/script-size/Sizer.hs
+++ onchain/test/script-size/Sizer.hs
@@ -7,6 +7,7 @@ module Sizer (
 
 import Data.String qualified as HString
 import Data.Tagged (Tagged (Tagged))
+import Plutonomy.UPLC qualified
 import Plutus.V1.Ledger.Scripts (Script (Script))
 import PlutusTx.Code (CompiledCode, sizePlc)
 import Test.Tasty (TestTree)
@@ -130,5 +131,30 @@ renderExcess tData mData diff =
     <> "Remaining headroom: "
     <> show diff
 
+-- Attempt to estimate the script size.  Note that:
+--
+--  * `programSize` counts the number of AST nodes in a program, which is very
+--     much different from the actual size of serialized script added to the
+--     transaction.
+--
+--  * We optimize the script before estiamting its size, because this gets us
+--    closer to how the scripts are being serialized for the off-chain code.
+--
+--  * Plutus abandoned CBOR serialization in favour of flat serialization. See
+--    https://github.com/input-output-hk/plutus/blob/master/doc/notes/plutus-core/Serialisation-flat.md
+--    for details.
+--
+--  * In order for any size approximation to make sense it has to be monotone.
+--    In other words, if we attempt to approximate the size of compiled scripts
+--    by AST size it must be the case that:
+--
+--       size(AST_1) < size(AST_2)  ==>  size(flat(AST_1)) <= size(flat(AST_2))
+--
+--    where "AST" is a UPLC program's AST, "size" computes the size, and "flat"
+--    performs flat encoding, or any other encoding used by cardano-node to
+--    represent transactions.  Note that we don't require strict monotonicity.
+--    If monotonicity doesn't hold then our size estimations will be useless,
+--    because then it can happen that making the AST size smaller will make the
+--    flat encoding larger.
 scriptSize :: Script -> Integer
-scriptSize (Script x) = UPLC.programSize x
+scriptSize (Script x) = UPLC.programSize (Plutonomy.UPLC.optimizeUPLC x)
