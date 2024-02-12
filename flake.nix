3d03fc1e3678e8fd8eddc85890a8a8b167fb1961
taylor.gunnoe@iohk.io <taylor.gunnoe@iohk.io>
2024-02-12 08:25:14 -0500

    [ETCM-5522] Push linux nix scopes to s3 cache

--- flake.nix
+++ flake.nix
@@ -5,10 +5,12 @@
     extra-substituters = [
       "https://cache.iog.io"
       "https://public-plutonomicon.cachix.org"
+      "https://cache.sc.iog.io"
     ];
     extra-trusted-public-keys = [
       "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="
       "public-plutonomicon.cachix.org-1:3AKJMhCLn32gri1drGuaZmFrmnue+KkKrhhubQk/CWc="
+      "cache.sc.iog.io:b4YIcBabCEVKrLQgGW8Fylz4W8IvvfzRc+hy0idqrWU="
     ];
   };
 
