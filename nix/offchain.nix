9f69030c10e9c2c42ae91503dc9085989b23dfdd
Taylor Gunnoe <taylor.gunnoe@iohk.io>
2024-08-12 08:20:47 -0400

    fix: use package locks for macos explicitely

--- nix/offchain.nix
+++ nix/offchain.nix
@@ -6,32 +6,42 @@
 ,
 }:
 let
-  offchain = repoRoot.nix.lib.patchedProject {
-    projectName = "sidechain-main-cli";
-    src = builtins.path {
-      path = ../offchain;
-      name = "sidechain-main-cli-src";
-      # TODO: Add more filters
-      filter = path: ftype: !(pkgs.lib.hasSuffix ".md" path);
+  offchain =
+    let
+      isDarwin = pkgs.lib.hasSuffix "darwin" system;
+    in
+    repoRoot.nix.lib.patchedProject {
+      projectName = "sidechain-main-cli";
+      src = builtins.path {
+        path = ../offchain;
+        name = "sidechain-main-cli-src";
+        # TODO: Add more filters
+        filter = path: ftype: !(pkgs.lib.hasSuffix ".md" path);
+      };
+
+      packageJson =
+        if isDarwin
+        then ../offchain/package-macos-aarch64.json
+        else ../offchain/package.json;
+      packageLock =
+        if isDarwin
+        then ../offchain/package-lock-macos-aarch64.json
+        else ../offchain/package-lock.json;
+      spagoPackages = ../offchain/spago-packages.nix;
+      withRuntime = true;
+      shell.packages = with pkgs; [
+        # Shell Utils
+        bashInteractive
+        git
+        jq
+
+        # Lint / Format
+        fd
+        dhall
+
+        # CTL Runtime
+        docker
+      ];
     };
-
-    packageJson = ../offchain/package.json;
-    packageLock = ../offchain/package-lock.json;
-    spagoPackages = ../offchain/spago-packages.nix;
-    withRuntime = true;
-    shell.packages = with pkgs; [
-      # Shell Utils
-      bashInteractive
-      git
-      jq
-
-      # Lint / Format
-      fd
-      dhall
-
-      # CTL Runtime
-      docker
-    ];
-  };
 in
 offchain
