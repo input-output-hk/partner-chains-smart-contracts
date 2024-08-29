08d228e1da072402a0482e49451fa29085bfeff3
Will Gould <will.gould@iohk.io>
2024-08-29 11:40:09 +0100

    don't need the type alias

--- offchain/src/TrustlessSidechain/CLIVersion.purs
+++ offchain/src/TrustlessSidechain/CLIVersion.purs
@@ -9,15 +9,13 @@ import Node.FS.Aff (readTextFile)
 import Node.Path (resolve)
 import Simple.JSON as JSON
 
-type PackageJson = { version ∷ String }
-
 getVersionString ∷ Aff String
 getVersionString = do
   packageJsonPath ← liftEffect $ resolve [] filename
   content ← readTextFile UTF8 packageJsonPath
   case JSON.readJSON content of
     Left err → pure $ errString <> show err
-    Right (pkg ∷ PackageJson) → pure ("Version: " <> pkg.version)
+    Right (pkg ∷ { version ∷ String }) → pure ("Version: " <> pkg.version)
   where
   filename = "package.json"
   errString = "Error: Unable to parse " <> filename <> ": "
