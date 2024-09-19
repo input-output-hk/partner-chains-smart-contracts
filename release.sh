a0cf1c0eb20a6d6aae7e66b98c0adcff1e0e3a7b
Krisztian Pinter <159046756+kpinter-iohk@users.noreply.github.com>
2024-09-19 16:48:02 +0200

    Release script improvements (#123)
    * release script improvements
    
    * address review comments
    
    * check for existing version tag
    
    * fix quote issue

--- release.sh
+++ release.sh
@@ -5,10 +5,37 @@ semver_pattern="^(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)(-((0|[1-9]\d*|\d*[a-zA
 packagejson=offchain/package.json
 changelog=CHANGELOG.md
 current_version=$(cat $packagejson | jq -r ".version")
+# shellcheck disable=SC2125
+cabalfile=onchain/*.cabal
+hsfiles=$(find onchain/src -type f -name "*.hs")
+
+if [[ -z $IN_NIX_SHELL ]]; then
+	echo "The release script must be run from inside a nix shell"
+	echo "    run 'nix develop' first"
+    exit 1
+fi
 
 if [[ ($# -eq 0) || $1 == "--help" || $1 == "-h" ]]; then
     echo "Usage: $0 [next-major|next-minor|next-patch|<semver>]"
     exit 1
+fi
+
+# check if there are any modified files (based on https://stackoverflow.com/a/3879077)
+git update-index --refresh &> /dev/null
+git diff-index --quiet HEAD --
+if [[ ($? -eq 1) ]]; then
+    echo "Release script can only run on a clean repo. Please stash your changes."
+    exit 1
+fi
+
+if [[ $(git rev-parse --abbrev-ref HEAD) != "master" ]]; then
+    echo "WARNING: you are trying to cut a release from a branch other than master!"
+    read -r -n1 -p "Do you wish to continue? [y/n]" yesno
+    printf "\n"
+    if [[ "$yesno" =~ ^[^Yy]$ ]]; then
+        echo "Aborting..."
+        exit 1
+    fi
 fi
 
 case $1 in
@@ -32,9 +59,24 @@ case $1 in
     ;;
 esac
 
+# if we didn't set next_version above we exit
 if [ -z "${next_version+x}" ]; then exit 1; fi
+
+# we check if release tag already exists
+if git show-ref --tags "v$next_version" --quiet; then
+    echo "ERROR: tag already exists for version!"
+    exit 1
+fi
 
 echo "Making release changes for new release $next_version (last version: $current_version)"
 
 cat $packagejson | jq ".version=\"$next_version\"" | sponge $packagejson
 sed -i "s/# Unreleased/# Unreleased\n\n# v$next_version/" $changelog
+# shellcheck disable=SC2086
+sed -i -r "s/^version:(\s*)\S+$/version:\1$next_version/" $cabalfile
+# shellcheck disable=SC2086
+sed -i -r "s/@since (U|u)nreleased/@since v$next_version/" $hsfiles
+
+git checkout -b "release-v$next_version"
+git add .
+git commit -m "chore: bump version to v$next_version"
