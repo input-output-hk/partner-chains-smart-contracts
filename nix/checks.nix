fcef73f00c72544dfb120ee47dcf9d475d2cdd82
Tobias Pflug <tobias.pflug@gmail.com>
2024-11-06 13:49:12 +0100

    ci: Use check-raw-scripts.sh
    Remove the previous nix-build based check of the RawScripts output and
    instead just invoke check-raw-scripts.sh

--- nix/checks.nix
+++ nix/checks.nix
@@ -17,64 +17,6 @@
     };
   };
 
-  upToDatePlutusScriptCheck = pkgs.runCommand "up-to-date-plutus-scripts-check"
-    {
-      nativeBuildInputs =
-        self.devShells.${pkgs.system}.default.nativeBuildInputs
-        ++ self.devShells.${pkgs.system}.default.buildInputs;
-    } ''
-    export LC_CTYPE=C.UTF-8
-    export LC_ALL=C.UTF-8
-    export LANG=C.UTF-8
-    export IN_NIX_SHELL='pure'
-
-    # Acquire temporary files..
-    TMP_PURS=$(mktemp)
-    TMP_RS=$(mktemp)
-
-    # Setup temporary files cleanup
-    function cleanup() {
-    rm -rf $TMP_PURS
-    }
-    trap cleanup EXIT
-
-    pushd ${self}/onchain > /dev/null
-    ${
-      self.packages."trustless-sidechain:exe:trustless-sidechain-serialise"
-    }/bin/trustless-sidechain-serialise \
-    --purescript-plutus-scripts="$TMP_PURS"
-    ${
-      self.packages."trustless-sidechain:exe:trustless-sidechain-serialise"
-    }/bin/trustless-sidechain-serialise \
-    --rust-plutus-scripts="$TMP_RS"
-    popd > /dev/null
-
-    pushd ${self}/offchain > /dev/null
-
-    # Compare the generated file and the file provided in the repo.
-    cmp $TMP_PURS src/TrustlessSidechain/RawScripts.purs || {
-    exitCode=$? ;
-    echo "Plutus scripts out of date." ;
-    echo 'See `offchain/src/TrustlessSidechain/RawScripts.purs` for instructions to resolve this' ;
-    exit $exitCode ;
-    }
-
-    popd > /dev/null
-
-    pushd ${self}/raw-scripts > /dev/null
-
-    # Compare the generated file and the file provided in the repo.
-    cmp $TMP_RS src/lib.rs || {
-    exitCode=$? ;
-    echo "Plutus scripts out of date." ;
-    echo 'See `raw-scripts/src/lib.rs` for instructions to resolve this' ;
-    exit $exitCode ;
-    }
-
-    popd > /dev/null
-
-    touch $out
-  '';
   partner-chains-smart-contracts =
     pkgs.runCommand "pc-contracts-check"
       {
