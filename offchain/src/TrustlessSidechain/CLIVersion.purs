5b7c1ccf2c175e176885a404ae6d28cdbcdaa5ec
jankun4 <michaljankun@gmail.com>
2025-01-24 18:18:09 +0100

    Remove offchain code

--- offchain/src/TrustlessSidechain/CLIVersion.purs
+++
@@ -1,9 +0,0 @@
-module TrustlessSidechain.CLIVersion (versionString) where
-
-import Prelude
-
-semVer :: String
-semVer = "__semVer" -- replaced by esbuild-plugin-replace to correct version
-
-versionString :: String
-versionString = "Version: " <> semVer
